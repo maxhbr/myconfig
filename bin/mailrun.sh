d09bf757360474ad75fe1530ae48b738d0fd74c9
Maximilian Huber <hubi@by-hubi.de>
2013-07-20 22:58:49 +0200

    aktualisiert

--- bin/mailrun.sh
+++ bin/mailrun.sh
@@ -6,14 +6,14 @@ if [[ ${pid} -gt 0 ]] ; then
   exit 1
 fi
 
-echo $$ > /tmp/mailrun.pid
+echo $$ > /tmp/mailrun-pid
 
 TIMEFILE=/tmp/mailrun-lastrun
 rm -f $TIMEFILE
 LOGFILE=/tmp/mailrun-log
-echo "**** started at $(date)****" >>$LOGFILE 2>&1
+echo "**** started at $(date) ****" >>$LOGFILE 2>&1
 
-## Monitor offlineimap
+# Monitor offlineimap #######################################################
 # checks every minute, if the logfile has changed in the last 5 minutes
 _monitor() {
   while true; do
@@ -23,30 +23,30 @@ _monitor() {
         # dead for more then 5 Minutes
         pid=$(pgrep -f "/usr/bin/offlineimap")
         if [[ ${pid} -gt 0 ]] ; then
-          echo "#############################################" >> $LOGFILE 2>&1
-          echo "#############################################" >> $LOGFILE 2>&1
-          echo "Offlineimap has hung with pid ${pid}" >> $LOGFILE 2>&1
+          echo "#############################################"
+          echo "#############################################"
+          echo "Offlineimap has hung with pid ${pid} (killed)"
           pkill -9 -f 'offlineimap\>.*-o'
-          echo "#############################################" >> $LOGFILE 2>&1
-          echo "#############################################" >> $LOGFILE 2>&1
+          echo "#############################################"
+          echo "#############################################"
         else
-          echo "#############################################" >> $LOGFILE 2>&1
-          echo "has hung, but there is no running offlineimap" >> $LOGFILE 2>&1
-          echo "#############################################" >> $LOGFILE 2>&1
+          echo "#############################################"
+          echo "has hung, but there is no running offlineimap"
+          echo "#############################################"
         fi
       fi
     fi
-  done
+  done >>$LOGFILE 2>&1
 }
 _monitor &
 #MONITOR_PID=$!
 trap 'kill $(jobs -p)' EXIT
 
 sleeptime=15 # sleep only 15s an the first run
-## main loop
+# main loop #################################################################
 while true; do
   sleep $sleeptime
-  sleeptime=60
+  sleeptime=120
   # check, if connected #####################################################
   if ! ping -c1 cloud.github.com > /dev/null 2>&1; then
     echo "**** not connected (ping) ****" >&2
@@ -56,20 +56,19 @@ while true; do
     fi
   fi
   # Run offlineimap #########################################################
-  echo "**** Offlineimap started (at $(date))****"
-  CURR=$(date '+%s')
+  echo "**** Offlineimap started (at $(date)) ****"
   if [[ $(pgrep -f "/usr/bin/offlineimap") -gt 0 ]] ; then
     echo "#############################################"
     echo "Offlineimap is already running with pid ${pid}"
     echo "#############################################"
   else
+    CURR=$(date '+%s')
     if [ ! -f $TIMEFILE ] || [ $(($CURR-$(cat $TIMEFILE))) -gt $((600)) ]; then
       echo "$CURR" >$TIMEFILE
       offlineimap -o -u ttyui
     else
       offlineimap -o -f INBOX -u ttyui
     fi
-    echo "**** Offlineimap is ready (at $(date))****"
+    echo "**** Offlineimap is ready (at $(date)) ****"
   fi
-  ###########################################################################
-done >> $LOGFILE 2>&1
+done >>$LOGFILE 2>&1
