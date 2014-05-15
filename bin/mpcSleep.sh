e43c742a7a599a0ce86aa528fe5085722b981f00
maximilianhuber <hubi@by-hubi.de>
2014-05-15 15:18:19 +0200

    automatisch aktualisiert

--- bin/mpcSleep.sh
+++ bin/mpcSleep.sh
@@ -1,41 +1,19 @@
 #!/bin/sh
 
-HOST="mpd@192.168.178.28"
-
-vol=`mpc -h $HOST | grep "volume" | awk '{print $2}'`
-vol=${vol%\%}
-base="60"
-
 mpcFade() {
-  for (( i = 0; i < $(($1-$2)); i++ )); do
-    mpc -h $HOST --no-status volume $(($1 - $i))
-    echo $(($1 - $i))
-    sleep 1
+  HOST="mpd@192.168.178.28"
+
+  t=$(($1 * 60))
+  vol=`mpc -h $HOST | grep "volume" | awk '{print $2}'`
+  vol=${vol%\%}
+  for (( i = 0; i < $vol; i++ )); do
+    mpc -h $HOST --no-status volume $(($vol - $i))
+    echo $(($vol - $i))
+    sleep $(($t / $vol))
   done
+  mpc -h $HOST --no-status pause
+  sleep 10
+  mpc -h $HOST --no-status volume $vol
 }
 
-sleep "$((6 * $base))"
-
-mpcFade $vol $(($vol / 2))
-sleep "$((4 * $base))"
-
-mpcFade $(($vol / 2)) $(($vol / 4))
-sleep "$((3 * $base))"
-
-mpcFade $(($vol / 4)) $(($vol / 6))
-sleep "$((2 * $base))"
-
-mpcFade $(($vol / 6)) $(($vol / 8))
-sleep "$((1 * $base))"
-
-mpcFade $(($vol / 8)) $(($vol / 10))
-sleep "$((1 * $base))"
-
-mpcFade $(($vol / 10)) $(($vol / 15))
-sleep "$((1 * $base))"
-
-mpcFade $(($vol / 10)) 0
-mpc -h $HOST --no-status pause
-sleep 10
-
-mpc -h $HOST --no-status volume $vol
+mpcFade 1
