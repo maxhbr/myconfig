53dde05ebac4652a29d5b2e180f56134b6a7fe8b
maximilianhuber <hubi@by-hubi.de>
2014-06-01 16:28:31 +0200

    automatisch aktualisiert

--- xmonad/myinformation.sh
+++ xmonad/myinformation.sh
@@ -22,14 +22,15 @@ case `expr $stngth / 10` in
 esac
 echo -n $essid $bar
 
-Mails1=$(find "$HOME/Mail/by-hubi/INBOX/new/" -type f | wc -l)
-Mails2=$(find "$HOME/Mail/alfa/INBOX/new/" -type f | wc -l)
+Mails1=$(find "$HOME/Mail/mail/INBOX/new/" -type f | wc -l)
+Mails2=$(find "$HOME/Mail/by-hubi/INBOX/new/" -type f | wc -l)
+Mails3=$(find "$HOME/Mail/alfa/INBOX/new/" -type f | wc -l)
 if test `find /tmp/mailrun-sh-log -mmin -2`; then
-  if ! [[ $Mails1 == "0" && $Mails2 == "0" ]]; then
-    echo -n "| <fc=#00ff00>Mail: $Mails1 / $Mails2</fc> "
+  if ! [[ $Mails1 == "0" && $Mails2 == "0" && $Mails3 == "0" ]]; then
+    echo -n "| <fc=#00ff00>Mail: ${Mails1}/${Mails2}/${Mails3}</fc> "
   fi
-elif ! [[ $Mails1 == "0" && $Mails2 == "0" ]]; then
-  echo -n "| <fc=#ff0000>no IMAP: $Mails1 / $Mails2</fc>"
+elif ! [[ $Mails1 == "0" && $Mails2 == "0" && $Mails3 == "0" ]]; then
+  echo -n "| <fc=#ff0000>no IMAP: ${Mails1}/${Mails2}/${Mails3}</fc>"
 else
   echo -n "| <fc=#ff0000>no IMAP</fc>"
 fi
