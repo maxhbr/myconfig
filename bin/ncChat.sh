a8bdc14674c7ee6752c639e032356a2cef18476d
maximilianhuber <mail@maximilian-huber.de>
2014-06-27 13:25:38 +0200

    automatisch aktualisiert

--- bin/ncChat.sh
+++ bin/ncChat.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # ~/bin/ncChat.sh
 #
-# uses ncat (or netcat or nc) for chatting over LAN
+# uses ncat (or netcat or nc) for encrypted chatting over LAN
 #
 # partly from: http://www.nixaid.com/linux/network/encrypted-chat-with-netcat
 #
 # written by maximilian-huber.de
-# Last modified: Mon Feb 03, 2014  10:00
+# Last modified: Fri Jun 27, 2014  01:25
 
-if [[ "$1" == "-h" ]]; then
+if [[ "$1" == "-h" ]] ||  [[ "$1" == "--help" ]]; then
   echo "ncChat.sh [-h] [-u] [-ip serverip] [passwd]"
   echo "   -h     Displays this message"
   echo "   -u     Use the username"
   echo "   -ip    Ip of the server, if none is given, a server will be started"
+  echo "* If no passwd is given, it uses a (bad) default password"
   echo "* Order of arguments is important"
   echo "* Needs ncat (or netcat or nc) to be installed"
   exit 0
@@ -27,42 +28,35 @@ echoDecodedMsg(){
 bold=`tput bold``tput setaf 6`
 normal=`tput sgr0`
 
-if [[ "$1" == "-u" ]]; then
+inPrefix=""
+if [[ "$1" == "-u" ]] ; then
   shift
   inPrefix="$(whoami): "
-else
-  inPrefix=""
 fi
 
 have ncat \
-  && { ncat="ncat "; } \
+  && { ncat="ncat"; } \
   || { have nc \
-    && { ncat="nc "; } \
+    && { ncat="nc"; } \
     || { have netcat  \
-      && {ncat="netcat "; } \
-      || { exit 1 }
-      }
-    }
+      && {ncat="netcat"; } \
+      || { exit 1 } ; } ; }
 
 if [[ "$1" == "-ip" ]]; then
   shift
-  ncat="${ncat}${1} 8877"
+  ncat="${ncat} ${1} 8877"
   shift
 else
-  ncat="${ncat}-l -p 8877"
+  ncat="${ncat} -l -p 8877"
 fi
 
+pass="DEFAULT_PASSWORD"
 if [[ "$#" -eq 1 ]]; then
-  #encrypted
-  pass=$1
-  while IFS= read -r userinput;do \
-      echo "$inPrefix$userinput" | openssl enc -aes-256-cbc -a -A -k $pass;echo;done \
-    | $ncat \
-    | while IFS= read -r srvOut;do echoDecodedMsg "$srvOut" "$pass"; done
-else
-  #not encrypted
-  while IFS= read -r userinput; do \
-      echo "$inPrefix$userinput" ; done \
-    | $ncat \
-    | while IFS= read -r srvOut; do echo "$bold$srvOut$normal"; done
+  pass=$1 
 fi
+
+while IFS= read -r userinput;do \
+    echo "($(date +'%H:%M')) $inPrefix$userinput" \
+      | openssl enc -aes-256-cbc -a -A -k $pass;echo;done \
+  | $ncat \
+  | while IFS= read -r srvOut;do echoDecodedMsg "$srvOut" "$pass"; done
