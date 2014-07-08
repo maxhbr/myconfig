bfe4e473465ad6e3600c1d9ccf7a77ca8862a36b
maximilianhuber <mail@maximilian-huber.de>
2014-07-08 13:17:18 +0200

    automatisch aktualisiert

--- bin/ncChat.sh
+++ bin/ncChat.sh
@@ -6,29 +6,46 @@
 # partly from: http://www.nixaid.com/linux/network/encrypted-chat-with-netcat
 #
 # written by maximilian-huber.de
-# Last modified: Tue Jul 08, 2014  12:57
+# Last modified: Tue Jul 08, 2014  01:15
 
 if [[ "$1" == "-h" ]] ||  [[ "$1" == "--help" ]]; then
-  echo "ncChat.sh [-h] [-u] [-w width] [-ip serverip] [passwd]"
+  echo "ncChat.sh -h"
   echo "   -h     Displays this message"
-  echo "   -u     Use the username"
+  echo "ncChat.sh [-u] [-w width] [-ip serverip] [passwd]"
+  echo "   -u     Use the username as prefix"
   echo "   -w     width of output"
   echo "   -ip    Ip of the server, if none is given, a server will be started"
+  echo
   echo "* If no passwd is given, it uses a (bad) default password"
   echo "* Order of arguments is important"
   echo "* Needs ncat (or netcat or nc) to be installed"
+  echo "* uses openssl for encryption"
+  echo
+  echo "written by maximilian-huber.de"
   exit 0
 fi
 
+###############################################################################
+# Global functions / variables
 have() { type "$1" &> /dev/null; }
 echoDecodedMsg(){
   echo "$bold$(echo "$1" | openssl enc -d -a -A -aes-256-cbc -k ${2})$normal" \
-    | fold -sw $width
+    | fold -sw width
 }
 
 bold=`tput bold``tput setaf 6`
 normal=`tput sgr0`
 
+have ncat \
+  && { ncat="ncat"; } \
+  || { have nc \
+    && { ncat="nc"; } \
+    || { have netcat  \
+      && {ncat="netcat"; } \
+      || { exit 1 } ; } ; }
+
+###############################################################################
+# Input parameter
 inPrefix=""
 if [[ "$1" == "-u" ]] ; then
   shift
@@ -42,14 +59,6 @@ if [[ "$1" == "-w" ]] ; then
   shift
 fi
 
-have ncat \
-  && { ncat="ncat"; } \
-  || { have nc \
-    && { ncat="nc"; } \
-    || { have netcat  \
-      && {ncat="netcat"; } \
-      || { exit 1 } ; } ; }
-
 if [[ "$1" == "-ip" ]]; then
   shift
   ncat="${ncat} ${1} 8877"
@@ -60,9 +69,11 @@ fi
 
 pass="DEFAULT_PASSWORD"
 if [[ "$#" -eq 1 ]]; then
-  pass=$1 
+  pass=$1
 fi
 
+###############################################################################
+# Main loop
 while IFS= read -r userinput;do \
     echo "($(date +'%H:%M')) $inPrefix$userinput" \
       | openssl enc -aes-256-cbc -a -A -k $pass;echo;done \
