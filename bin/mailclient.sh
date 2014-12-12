52d783c4a4e3f44ab4db9a709629ffe3f09b3931
maximilianhuber <mail@maximilian-huber.de>
2014-12-12 22:57:43 +0100

    some changes

--- bin/mailclient.sh
+++ bin/mailclient.sh
@@ -9,5 +9,5 @@ if tmux has-session -t "mutt" 2>/dev/null; then
   tmux attach -d -t "mutt"
 else
   #urxvt -e bash -c "tmux new-session -s \"${name}\" \"${file_path}\" \; set-option status \; set set-titles-string \"${name} (tmux@${HOST})\""
-  tmux new-session -s "mutt" "command mutt" \; set-option status \; set set-titles-string "mutt (tmux@x230)"
+  tmux -2 new-session -s "mutt" "command mutt" \; set-option status \; set set-titles-string "mutt (tmux@x230)"
 fi
