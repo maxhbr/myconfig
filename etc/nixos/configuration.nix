e70ea115063ce7b26441127653b41c00fd07ffc7
maximilianhuber <mail@maximilian-huber.de>
2015-03-30 22:02:26 +0200

    some small changes on the nix cfg

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,32 +1,21 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 let
   hsPackages = with pkgs.haskellPackages; [
-    xmonad
-    xmobar
-    ghc
-    hlint
-    pandoc
-    pointfree
-    pointful
+    xmonad xmobar
+    ghc hlint pandoc pointfree pointful
   ];
 in {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub = {
     enable = true;
     version = 2;
     device = "/dev/sda";
   };
-  # Define on which hard drive you want to install Grub.
 
   networking = {
     hostName = "nixos"; # Define your hostname.
@@ -34,7 +23,6 @@ in {
     # wireless.enable = true;  # Enables wireless.
   };
 
-  # Select internationalisation properties.
   i18n = {
     consoleFont = "lat9w-16";
     consoleKeyMap = "de";
@@ -45,10 +33,9 @@ in {
   # $ nix-env -qaP | grep wget
   environment = {
     systemPackages = with pkgs; [
-      wget pkgs.htop
-      vim zsh tmux
+      wget htop vim zsh tmux
       gitAndTools.gitFull
-      xlibs.xmodmap
+      xlibs.xmodmap xlibs.xset
       dmenu scrot unclutter feh redshift
       rxvt roxterm chromium
     ] ++ hsPackages;
@@ -69,7 +56,7 @@ in {
     printing.enable = true;
     xserver = {
       enable = true;
-      layout = "de";
+      layout = "de"; # TODO: neo
       # xkbOptions = "eurosign:e";
       windowManager = {
         xmonad = {
@@ -80,13 +67,24 @@ in {
       };
       desktopManager.default = "none";
       displayManager.sessionCommands = ''
+        ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr &
         ${pkgs.redshift}/bin/redshift -l 48.2:10.8 &
         ${pkgs.roxterm}/bin/roxterm &
       '';
+
+      # synaptics.additionalOptions = ''
+      #   Option "VertScrollDelta" "-100"
+      #   Option "HorizScrollDelta" "-100"
+      #   '';
+      # synaptics.buttonsMap = [ 1 3 2 ];
+      # synaptics.enable = true;
+      # synaptics.tapButtons = false;
+      # synaptics.fingersMap = [ 0 0 0 ];
+      # synaptics.twoFingerScroll = true;
+      # synaptics.vertEdgeScroll = false;
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.mhuber = {
     isNormalUser = true;
     group = "users";
@@ -98,3 +96,5 @@ in {
   };
 
 }
+
+# vim:set ts=2 sw=2 sts=2 et foldmethod=marker foldlevel=0 foldmarker={{{,}}}:
