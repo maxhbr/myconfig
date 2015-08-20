4e5ad5a2d1db1aad4e46a53476e869bbf4d21202
maximilianhuber <mail@maximilian-huber.de>
2015-08-20 11:56:58 +0200

    update nixos config

--- etc/nixos/common.nix
+++ etc/nixos/common.nix
@@ -51,11 +51,15 @@ in {
       ./hardware-configuration.nix
     ];
 
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    device = "/dev/sda";
-  };
+  boot = {
+    cleanTmpDir = true;
+    loader.grub = {
+      enable = true;
+      version = 2;
+      device = "/dev/sda";
+      memtest86.enable = true;
+    };
+  }
 
   networking = {
     hostName = "nixos"; # Define your hostname.
@@ -95,6 +99,7 @@ in {
 
   services = {
     openssh.enable = true;
+    ntp.enable = true;
     printing.enable = true;
     xserver = {
       enable = true;
@@ -127,9 +132,15 @@ in {
       # synaptics.fingersMap = [ 0 0 0 ];
       # synaptics.twoFingerScroll = true;
       # synaptics.vertEdgeScroll = false;
+
+      acpid.enable = true;
     };
 
     nixosManual.showManual = true;
+
+
+    # TODO: emacs service?
+    # see: https://github.com/ardumont/dot-files/blob/master/nixos/services.nix#L28
   };
 
   users = {
@@ -137,15 +148,23 @@ in {
       isNormalUser = true;
       group = "users";
       uid = 1000;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel" "audio" "video" "vboxusers" "docker" "networkmanager" "dialout" ];
       createHome = true;
       home = "/home/mhuber";
       shell = "/run/current-system/sw/bin/zsh";
+      password = "dummy"
     };
     extraGroups.docker.members = [ "mhuber" ];
   };
   virtualisation.docker.enable = true;
+  # services.virtualboxHost.enable = false;
   programs.zsh.enable = true;
+
+
+  system.activationScripts.media = ''
+    mkdir -m 0755 -p /media /share
+  '';
+
 }
 
 # vim:set ts=2 sw=2 sts=2 et foldmethod=marker foldlevel=0 foldmarker={{{,}}}:
