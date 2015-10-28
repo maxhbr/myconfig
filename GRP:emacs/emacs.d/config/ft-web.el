27b83691cf1df60256796155a619abb373e9e476
maximilianhuber <maximilian.huber@tngtech.com>
2015-10-28 13:23:35 +0100

    automatic commit for emacs

--- GRP:emacs/emacs.d/config/ft-web.el
+++ GRP:emacs/emacs.d/config/ft-web.el
@@ -1,12 +1,22 @@
-;; (use-package php-mode
-;;   :ensure t
-;;   :mode "\\.php\\'")
-;; (use-package web-mode
-;;   :ensure t
-;;   :mode "\\.php\\'")
+;; (use-package flymake
+;;   :config
+;;   (defun flymake-php-init ()
+;;     "Use php to check the syntax of the current file."
+;;     (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+;;            (local (file-relative-name temp (file-name-directory buffer-file-name))))
+;;       (list "php" (list "-f" local "-l"))))
+
+;;   (add-to-list 'flymake-err-line-patterns
+;;                '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+;;   (add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+
+;;   (add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
+;;   ;; (define-key php-mode-map '[M-S-up] 'flymake-goto-prev-error)
+;;   ;; (define-key php-mode-map '[M-S-down] 'flymake-goto-next-error)
+;;   )
 
 (use-package php-mode
-  :init
   :config
   (add-hook 'php-mode-hook (lambda ()
                              (setq indent-tabs-mode nil
@@ -14,11 +24,25 @@
                                    tab-width 2)))
   (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
   (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode)) 
+  (use-package flymake-easy
+    :ensure t
+    :config
+    (use-package flymake-php
+      :ensure t
+      :config
+      (add-hook 'php-mode-hook 'flymake-php-load)))
   :ensure t)
 
 (use-package web-mode
   :config
-  (add-to-list 'auto-mode-alist '("\\.php.twig$" . web-mode))
+  (add-hook 'web-mode-hook (lambda ()
+                             (setq indent-tabs-mode nil
+                                   c-basic-offset 2
+                                   tab-width 2)))
+  ;; (add-to-list 'auto-mode-alist '("\\.php.twig$" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.xml.twig$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
   :ensure t)
 
 (defun my-php ()
