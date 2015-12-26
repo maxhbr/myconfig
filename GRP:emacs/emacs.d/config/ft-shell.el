9f47d1a686e1df769d6869f4420dce0954c63874
maximilianhuber <maximilian.huber@tngtech.com>
2015-12-26 23:23:22 +0100

    automatic commit for emacs

--- GRP:emacs/emacs.d/config/ft-shell.el
+++ GRP:emacs/emacs.d/config/ft-shell.el
@@ -1,5 +1,7 @@
-
 (add-hook 'sh-mode '(lambda ()
                       (load "folding" 'nomessage 'noerror)
                       (folding-mode-add-find-file-hook)
                       (folding-add-to-marks-list 'sh-mode "{{{" "}}}" nil t)))
+
+(add-hook 'sh-mode-hook (lambda () (setq tab-width 2 sh-basic-offset 2 indent-tabs-mode t)))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
