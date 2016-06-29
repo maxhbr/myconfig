1560a11c40a0284fc58e4f837a7892e72dcdcee9
maximilianhuber <maximilian.huber@tngtech.com>
2016-06-29 15:20:31 +0200

    automatic commit for mutt

--- GRP:mutt/bin/mailclient.sh
+++ GRP:mutt/bin/mailclient.sh
@@ -6,8 +6,16 @@ if tmux has-session -t "mutt" 2>/dev/null; then
   #[[ $- != *i* ]] && return
 
   #urxvt -e bash -c "tmux attach -d -t ${name}"
-  tmux attach -d -t "mutt"
+  tmux attach -d -t "mutt" \; \
+       split-window -l 2 'offlineimap -o 2>&1 | grep --color=always "Copy message" && exit' \; \
+       last-pane
 else
   #urxvt -e bash -c "tmux new-session -s \"${name}\" \"${file_path}\" \; set-option status \; set set-titles-string \"${name} (tmux@${HOST})\""
-  tmux -2 new-session -s "mutt" "command mutt" \; set-option status \; set set-titles-string "mutt (tmux@x230)"
+  tmux -2 \
+       new-session -s "mutt" "command mutt" \; \
+       set-option status \; \
+       set set-titles-string "mutt (tmux@x230)" \; \
+       split-window -l 2 'echo "sync..."; offlineimap -o 2>&1 | grep --color=always "Copy message" && exit' \; \
+       last-pane \;
 fi
+
