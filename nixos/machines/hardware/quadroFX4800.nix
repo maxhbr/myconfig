d537e591a99aca2ee9c1e1268f513065b39b1caf
maximilianhuber <mail@maximilian-huber.de>
2017-04-14 18:48:31 +0200

    nvidia drive fails... switch to alternative

--- nixos/machines/hardware/quadroFX4800.nix
+++ nixos/machines/hardware/quadroFX4800.nix
@@ -4,5 +4,6 @@
 #   GeForce Quadro FX 4800
 
 {
-  services.xserver.videoDrivers = ["nvidiaLegacy340"];
+  #services.xserver.videoDrivers = ["nvidiaLegacy340"];
+  services.xserver.videoDrivers = ["nouveau"];
 }
