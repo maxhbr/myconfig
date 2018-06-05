0953d50696ce9c0e176859bb4bb556a45dab2eae
Maximilian Huber <mail@maximilian-huber.de>
2018-06-05 10:50:44 +0200

    nixpkgs have catched up with idea

--- nix/overlays/idea-ultimate.nix
+++ nix/overlays/idea-ultimate.nix
@@ -2,20 +2,20 @@
 # SPDX-License-Identifier: MIT
 self: super:
 let
-  name = "idea-ultimate-${version}";
   version = "2018.1.4";
   sha256 = "1122dhk7ad3a8iw7fs55kg7cjj657hcxbk2ykhd3x0h2759y25pv";
-  oldVersion = "2017.2.5"; # super.lib.getVersion super.idea.idea-ultimate;
-  overlayIsNewer =  super.lib.versionOlder oldVersion version;
+  pkgsVersion = "2018.1.4"; # super.lib.getVersion super.idea.idea-ultimate;
+  overlayIsNewer =  super.lib.versionOlder pkgsVersion version;
 in if overlayIsNewer
    then {
      idea-ultimate = super.idea.idea-ultimate.overrideAttrs ( oldAttrs: {
-       inherit name version;
+       inherit version;
+       name = "idea-ultimate-${version}";
        src = super.fetchurl {
          url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
          inherit sha256;
        };
      });
    } else {
-    idea-ultimate = super.idea.idea-ultimate;
+     idea-ultimate = super.idea.idea-ultimate;
    }
