b7e39d76cd32c2b98ebc427c655c4d27e7a20f69
Maximilian Huber <mail@maximilian-huber.de>
2018-06-05 21:09:14 +0200

    add script to bump idea version

--- nix/overlays/idea-ultimate/default.nix
+++ nix/overlays/idea-ultimate/default.nix
@@ -2,18 +2,18 @@
 # SPDX-License-Identifier: MIT
 self: super:
 let
-  version = "2018.1.4";
-  sha256 = "1122dhk7ad3a8iw7fs55kg7cjj657hcxbk2ykhd3x0h2759y25pv";
+  jsonFile = ./idea-ultimate.json;
+  json = builtins.fromJSON (builtins.readFile jsonFile);
   pkgsVersion = "2018.1.4"; # super.lib.getVersion super.idea.idea-ultimate;
-  overlayIsNewer =  super.lib.versionOlder pkgsVersion version;
+  overlayIsNewer =  super.lib.versionOlder pkgsVersion json.version;
 in if overlayIsNewer
    then {
      idea-ultimate = super.idea.idea-ultimate.overrideAttrs ( oldAttrs: {
-       inherit version;
-       name = "idea-ultimate-${version}";
+       inherit (json) version;
+       name = "idea-ultimate-${json.version}";
        src = super.fetchurl {
-         url = "https://download.jetbrains.com/idea/ideaIU-${version}.tar.gz";
-         inherit sha256;
+         url = "https://download.jetbrains.com/idea/ideaIU-${json.version}.tar.gz";
+         inherit (json) sha256;
        };
      });
    } else {
