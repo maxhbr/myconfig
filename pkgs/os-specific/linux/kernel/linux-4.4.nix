51c73ce88c6c739b0320a0b1d8150b24645b4637
Maximilian Huber <mail@maximilian-huber.de>
2018-07-30 08:13:54 +0200

    Squashed 'nix/nixpkgs/' changes from d6c6c7fcec6..a0850041107
    a0850041107 jetbrains.ruby-mine: 2018.1.4 -> 2018.2
    9116f941378 jetbrains.phpstorm: 2018.1.6 -> 2018.2
    638f70f2f15 jetbrains.datagrip: 2018.1.5 -> 2018.2
    5b231c2a9e7 jetbrains.clion: fix paths to patched binaries and fix gdb binary
    f9d6486aa5b jetbrains.goland: 2018.1.5 -> 2018.2
    4c641a0b61c jetbrains.rider: 2018.1.2 -> 2018.1.3
    db0cf2a9c21 jetbrains.pycharm-professional: 2018.1.4 -> 2018.2
    ea947194495 jetbrains.pycharm-community: 2018.1.4 -> 2018.2
    75937fc0bf9 jetbrains.idea-ultimate: 2018.1.6 -> 2018.2
    7ccf6002438 jetbrains.idea-community: 2018.1.6 -> 2018.2
    c34b1295fd1 jetbrains.clion: 2018.1.6 -> 2018.2
    5bca1a2bbd8 jetbrains.webstorm: 2018.1.5 -> 2018.2
    64afbfaf147 jetbrains.ruby-mine: 2018.1.3 -> 2018.1.4
    dd5644acea1 jetbrains.idea-ultimate: 2018.1.5 -> 2018.1.6
    8db9f1d9a59 jetbrains.idea-community: 2018.1.5 -> 2018.1.6
    33a97cb148c jetbrains.goland: 2018.1.4 -> 2018.1.5
    4133dd72035 jetbrains.datagrip: 2018.1.4 -> 2018.1.5
    11fa42880b2 jetbrains.clion: 2018.1.5 -> 2018.1.6
    6ffbde26ad7 openjdk: 8u172-b11 -> 8u181-b13
    78029b84ddb oh-my-zsh: 2018-04-25 -> 2018-07-29
    f4990255a30 php: 7.2.7 -> 7.2.8 (#43740)
    30f7cd981f3 Merge pull request #44163 from YorikSar/update-chromium-18.03
    84d9355dde7 Merge pull request #44055 from ljli/vscode-fix-stable
    eee71d85d5f linux: 4.17.10 -> 4.17.11
    a156001a7df linux: 4.14.58 -> 4.14.59
    d08d4ec396a linux: 4.9.115 -> 4.9.116
    71011e1f4f4 linux: 4.4.144 -> 4.4.145
    acc69710447 Merge pull request #44169 from samueldr/backport/18.03/xx-0009ae3d
    92262dffe52 onboard: fix missing typelibs
    8fbae9da23c chromium: 67.0.3396.99 -> 68.0.3440.75
    59c297c8558 chromium: fix 68 (beta) build
    038263eedc9 chromium: 67.0.3396.87 -> 67.0.3396.99
    95aa869f04c pythonPackages.rfc7464: Init at 17.7.0
    7d85757bc37 Merge pull request #44090 from balsoft/release-18.03
    adf8df1bb13 linux: 4.17.9 -> 4.17.10
    1fa84d3a4b7 linux: 4.14.57 -> 4.14.58
    f682d0050fb linux: 4.9.114 -> 4.9.115
    f0edaf7e72e linux: 4.4.143 -> 4.4.144
    0bf38518637 Fixed merge
    7bb2300026a Fix libreoffice theme
    afffbe50fbb EC2 AMIs: 18.03.131792.becbe4dbe16 -> 18.03.132946.1caae7247b8
    dba78f04c25 create-amis.sh: Change directory for AMIs
    8582bf5e3ef vscode: fix keymapping.node rpath
    1caae7247b8 Merge pull request #44031 from andir/18.03/networkmanager-vpnc
    858513635f1 networkmanager-vpnc: 1.2.4 -> 1.2.6
    3cf06212b00 neovim: fix the xsel path in the clipboard runtime provider
    d6691935ebb Merge pull request #44026 from andir/18.03/mutt
    2a45ea1dba3 mutt: 1.9.4 -> 1.10.1
    917906a23e2 Merge pull request #44024 from andir/18.03/neomutt
    3e6c2583ca3 neomutt: 20180223 -> 20180716
    5b190ea8ce8 skypeforlinux: 8.18.0.6 -> 8.24.0.2
    cb6b846a12b skypeforlinux: remove libstdc++5 dependency (#41590)
    38c28b34d9d skypeforlinux: gnome2 cleanup (#41176)
    bd834a56538 Merge pull request #44005 from Mic92/gitea-backport
    e850302491d gitea: 1.4.2 -> 1.4.3
    d1d8c12f012 gitea: 1.4.1 -> 1.4.2
    42c35dea37b nixos/gitea: fix pre start script
    36472c15265 gitea: 1.4.0 -> 1.4.1
    1b7d8567d56 gitea: 1.3.3 -> 1.4.0
    dea932eabf3 linux: 4.17.8 -> 4.17.9
    3c61f44aceb linux: 4.14.56 -> 4.14.57
    83e93c76f3d linux: 4.9.113 -> 4.9.114
    cf584f5da3c linux: 4.4.142 -> 4.4.143
    e20106f31ad Merge #43538: fetchpatch: quote excludes
    c7ac94bd0c9 gtkspell3: add gir
    1f1aa5f888d eolie: fix build
    7ee55ebf162 Merge pull request #43667 from FRidh/1803_python36
    6115f477703 postgresql100: 10.3 -> 10.4
    1eefe30e030 postgresql96: 9.6.8 -> 9.6.9
    88c6f0ca34c postgresql95: 9.5.12 -> 9.5.13
    0c238624a99 postgresql94: 9.4.17 -> 9.4.18
    129ae17266a postgresql93: 9.3.22 -> 9.3.23
    99adfd0a422 nixos tests: bring back non-blocking nfs tests
    634c7e62cd5 python36: 3.6.5 -> 3.6.6
    
    git-subtree-dir: nix/nixpkgs
    git-subtree-split: a0850041107e3442316f843ae00e4be22f64cfb9

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -1,11 +1,11 @@
 { stdenv, buildPackages, hostPlatform, fetchurl, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  version = "4.4.142";
+  version = "4.4.145";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0zyxlqjnxrr1a1wlg3hzk8sx77ysmy66wb34kp77iv04xr9p9kai";
+    sha256 = "1max0d1b1y1ndrfprrcyb7c9y12pkx2whxzlr70qypcb5jz0v7ff";
   };
 } // (args.argsOverride or {}))
