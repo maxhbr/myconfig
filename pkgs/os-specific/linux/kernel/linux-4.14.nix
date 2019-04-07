2e3ac72916d28141895d8f3c4de786a561ff1950
Maximilian Huber <gh@maxhbr.de>
2019-04-07 17:53:52 +0200

    Squashed 'nix/nixpkgs/' changes from 3eed6d45739..f9576d0cce2
    f9576d0cce2 youtube-dl: 2019.04.01 -> 2019.04.07
    680b155975c linux: 4.9.167 -> 4.9.168
    bff31dd80d6 linux: 4.19.33 -> 4.19.34
    46b460ef2a2 linux: 4.14.110 -> 4.14.111
    222950952f1 postfix: 3.3.3 -> 3.3.4 (#58680)
    58efb6980fb Merge pull request #58967 from aanderse/apacheHttpd-2.4.39
    f2b553e8100 apacheHttpd: 2.4.35 -> 2.4.39
    6ae427dee0b linux: 4.19.32 -> 4.19.33
    f3543d07f0e linux: 4.14.109 -> 4.14.110
    0c2cc7f921f linux: 4.9.166 -> 4.9.167
    02ab2b87724 linux: 4.4.177 -> 4.4.178
    1d36ad6d16d youtube-dl: 2019.03.18 -> 2019.04.01
    884b0a4417e Merge pull request #58828 from obsidiansystems/km-fix-androidndk-cc-wrapper-flags
    7afd5e3e7a1 Remove unrecognized "float" settings for Android aarch32 systems
    5f4bf240f51 androidndk-pkgs: Fix cc-wrapper flags
    58fbebc4ea5 browserpass: Fix evaluation
    e2a1fd1172b Merge pull request #58770 from primeos/security-backports-for-18.09
    216f062a61f gollum: Update the dependencies (security)
    02aceb65cc1 gollum: rack: 1.6.10 -> 1.6.11
    70d8066e565 html-proofer: 3.8.0 -> 3.10.2 (security)
    b7910443a73 nixos/browserpass: update for v3
    d22d43ac196 browserpass: 2.0.22 -> 3.0.1
    43ba99fa035 notary: disable tests (for now)
    
    git-subtree-dir: nix/nixpkgs
    git-subtree-split: f9576d0cce20084dfe1a4dcb9544ee912a1988d3

--- pkgs/os-specific/linux/kernel/linux-4.14.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14.nix
@@ -3,16 +3,15 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.14.109";
+  version = "4.14.111";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
-  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1q4fv07kl07yyi8173fdx7m98pdn3mz8z3a6y52qfmkryxjz2r1p";
+    sha256 = "1s56819kkr7h48njk708f9gapy2hr97vxawp5qflv1izamb7s6gq";
   };
 } // (args.argsOverride or {}))
--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,16 +3,15 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.32";
+  version = "4.19.34";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
-  modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = concatStrings (intersperse "." (take 2 (splitString "." version)));
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "1ng730lz7riiq7ja4k3x17njs9779jy49b15kbfy7bi48cax29m3";
+    sha256 = "1k5qhyh7nwfs5pkbrjpxyj6w17424qcmmd9v7jqfbgnx3wm5wyfx";
   };
 } // (args.argsOverride or {}))
