e383648ca15af003364e4a44f7d56f0e1c88673a
Maximilian Huber <gh@maxhbr.de>
2019-04-17 09:10:58 +0200

    improve photo-scripts/convertFromRaw.sh
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- photo-scripts/convertFromRaw.sh
+++ photo-scripts/convertFromRaw.sh
@@ -5,22 +5,45 @@
 # Written by Sergey Mashchenko
 # links: https://pulsar124.fandom.com/wiki/Open_Source_workflow_for_macro_focus_stacking
 #        https://github.com/pulsar123/Macro-scripts
+# manpage: https://www.cybercom.net/~dcoffin/dcraw/dcraw.1.html
 
 set -e
 
 if [[ "$1" == "--help" ]]; then
     cat<<EOF
-  $0 [-l] raw [raw [raw ...]]
+  $0 [-l|-default] [-q0|-q1|-q2|-q3] raw [raw [raw ...]]
 EOF
     exit 0
 fi
 
+# choose color space
+# sRGB is the default
 if [[ "$1" == "-l" ]]; then
-     echo "Using linear color space"
+    shift
+    echo "Using linear color space"
     opts="-4"
+    # Linear 16-bit, same as -6 -W -g 1 1
+elif [[ "$1" == "-default" ]]; then
+    echo "Using default color space"
+    opts=""
 else
     echo "Using sRGB color space"
-    opts="-6 -W"
+    opts="-6 -W -g 2.4 12.92"
+fi
+
+# choose quality (default is -q3)
+if [[ "$1" == "-q0" ]]; then
+    shift
+    opts="$opts -q 0"
+elif [[ "$1" == "-q1" ]]; then
+    shift
+    opts="$opts -q 1"
+elif [[ "$1" == "-q2" ]]; then
+    shift
+    opts="$opts -q 2"
+elif [[ "$1" == "-q3" ]]; then
+    shift
+    opts="$opts -q 3"
 fi
 
 # RAW images conversion to 48-bit TIFFs:
