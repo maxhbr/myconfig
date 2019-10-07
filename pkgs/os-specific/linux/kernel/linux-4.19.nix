c5141a729dc34f1956d5c08282f3e51595998d41
Maximilian Huber <gh@maxhbr.de>
2019-10-07 19:54:04 +0200

    Squashed 'nix/nixpkgs/' changes from 6420e2649fa..0e0ee084d6d
    0e0ee084d6d linuxPackages.virtualBoxGuestAdditions: fix build
    4cf8a797ea5 nixos/doc: fix manpage format
    146425a5909 linux: 5.2.18 -> 5.2.19
    b52cca16fc3 linux: 4.9.194 -> 4.9.195
    bf7844d5d6f linux: 4.4.194 -> 4.4.195
    b48b4ca7a0b linux: 4.19.76 -> 4.19.77
    fcd7b863ba1 linux: 4.14.146 -> 4.14.147
    b0453306435 libpng12: 1.2.57 -> 1.2.59
    bcc3c118bfa unbound: patch CVE-2019-16866 (DoS, minor one IMHO)
    ec1070d4d40 Merge pull request #70288 from ttuegel/security--release-19.03--CVE-2019-14744
    949da84383a pykde4: Disable build on Hydra
    665e6717173 kdelibs: vulnix: patch name is CVE number
    3e7cb73856f kconfig: vulnix: patch name is CVE number
    a1ccf5ff6ab Merge pull request #70263 from ttuegel/security--release-19.03--CVE-2019-14744
    3879ac8b90e pykde4: Patch for CVE-2019-14744
    a7417ebf45a kconfig: Patch for CVE-2019-14744
    7035462ddef ruby_2_6: 2.6.2 -> 2.6.5
    c8e7fec546b ruby_2_5: 2.5.5 -> 2.5.7
    0dbd38cac0d ruby_2_4: 2.4.5 -> 2.4.9
    a9c15710b9f exim: 4.92.2 -> 4.92.3
    f9206f287e7 linux: 5.2.17 -> 5.2.18
    9aa95fd69f4 linux: 4.19.75 -> 4.19.76
    
    git-subtree-dir: nix/nixpkgs
    git-subtree-split: 0e0ee084d6dfc83b5b67b6cbcefe43664114808d

--- pkgs/os-specific/linux/kernel/linux-4.19.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "4.19.75";
+  version = "4.19.77";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStrings (intersperse "." (take 3 (splitString "." "${version}.0"))) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
-    sha256 = "0y0vcmxyfg98mm63vaqq6n2bmxkbmrnvigm5zdh1al74w53p2pnx";
+    sha256 = "1agksl35amjzc6g6d9zjwz6p5qir2cligb5c1d9s2bag9766jav1";
   };
 } // (args.argsOverride or {}))
