b638084a327992ffe27cc8d4a38a6ca3bf939b5e
Maximilian Huber <gh@maxhbr.de>
2019-11-01 20:43:16 +0100

    on the way to x1extremeG2
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- nixos/machines/x1extremeG2.nix
+++ nixos/machines/x1extremeG2.nix
@@ -27,7 +27,7 @@
 
   boot.initrd.supportedFilesystems = [ "luks" ];
   boot.initrd.luks.devices = [{
-    device = "/dev/disk/by-uuid/fc9ecff5-e0c5-4cff-bb5c-08a745c76e3c";
+    device = "/dev/disk/by-uuid/0b3b85df-65ef-4b2b-8073-a62c5a7346dc";
     name = "crypted";
     preLVM = true;
     allowDiscards = true;
