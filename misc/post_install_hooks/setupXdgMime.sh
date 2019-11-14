afddd9d0f0485f32cb58906692ed809fe63f131a
Maximilian Huber <gh@maxhbr.de>
2019-11-14 20:53:16 +0100

    update before rebuild.sh

--- misc/post_install_hooks/setupXdgMime.sh
+++ misc/post_install_hooks/setupXdgMime.sh
@@ -1,17 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p xdg_utils
 # Copyright 2019 Maximilian Huber <oss@maximilian-huber.de>
 # SPDX-License-Identifier: MIT
 set -e
 
-setup() {
+setupXdgMime() {
     mimetype="$1"
     app="${2}.desktop"
 
-    if [[ "$(xdg-mime query default application/pdf)" != *"$app"* ]]; then
+    echo "setup $app for $mimetype"
+    if [[ "$(xdg-mime query default "$mimetype")" != *"$app"* ]]; then
         set -x
-        xdg-mime default "$app" application/pdf
+        xdg-mime default "$app" "$mimetype"
         set +x
+    else
+        echo "... already done"
     fi
 }
 
-setup "application/pdf" mupdf
+setupXdgMime application/pdf mupdf
+setupXdgMime image/jpeg sxiv
+setupXdgMime image/png sxiv
