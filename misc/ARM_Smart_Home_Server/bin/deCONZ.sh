904a5769aba2ca5288a6f12fe28bedad0af7b157
Maximilian Huber <gh@maxhbr.de>
2020-01-01 18:08:05 +0100

    fix deCONZ.sh
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- misc/ARM_Smart_Home_Server/bin/deCONZ.sh
+++ misc/ARM_Smart_Home_Server/bin/deCONZ.sh
@@ -27,29 +27,29 @@ update() {
     sudo docker stop deconz && sudo docker rm deconz || true
     sudo docker pull marthoc/deconz:latest
 }
-updateFirmware() {
-#     Updating Conbee/RaspBee Firmware
+# updateFirmware() {
+# #     Updating Conbee/RaspBee Firmware
 
-# Firmware updates from the web UI will fail silently. Instead, an interactive utility script is provided as part of this Docker image that you can use to flash your device's firmware. The script has been tested and verified to work for Conbee on amd64 Debian linux and armhf Raspbian Stretch and RaspBee on armhf Raspbian Stretch. To use it, follow the below instructions:
+# # Firmware updates from the web UI will fail silently. Instead, an interactive utility script is provided as part of this Docker image that you can use to flash your device's firmware. The script has been tested and verified to work for Conbee on amd64 Debian linux and armhf Raspbian Stretch and RaspBee on armhf Raspbian Stretch. To use it, follow the below instructions:
 
-#     Check your deCONZ container logs for the update firmware file name: type docker logs [container name], and look for lines near the beginning of the log that look like this, noting the .CGF file name listed (you'll need this later):
+# #     Check your deCONZ container logs for the update firmware file name: type docker logs [container name], and look for lines near the beginning of the log that look like this, noting the .CGF file name listed (you'll need this later):
 
-#     GW update firmware found: /usr/share/deCONZ/firmware/deCONZ_Rpi_0x261e0500.bin.GCF
-#     GW firmware version: 0x261c0500
-#     GW firmware version shall be updated to: 0x261e0500
+# #     GW update firmware found: /usr/share/deCONZ/firmware/deCONZ_Rpi_0x261e0500.bin.GCF
+# #     GW firmware version: 0x261c0500
+# #     GW firmware version shall be updated to: 0x261e0500
 
-#     docker stop [container name] or docker-compose down to stop your running deCONZ container (you must do this or the firmware update will fail).
+# #     docker stop [container name] or docker-compose down to stop your running deCONZ container (you must do this or the firmware update will fail).
 
-#     Invoke the firmware update script: docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules -v /sys:/sys marthoc/deconz
+# #     Invoke the firmware update script: docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules -v /sys:/sys marthoc/deconz
 
-#     Follow the prompts:
+# #     Follow the prompts:
 
-#     Enter the path (e.g. /dev/ttyUSB0) that corresponds to your device in the listing.
-#     Type or paste the full file name that corresponds to the file name that you found in the deCONZ container logs in step 1 (or, select a different filename, but you should have a good reason for doing this).
-#     If the device/path and file name look OK, type Y to start flashing!
+# #     Enter the path (e.g. /dev/ttyUSB0) that corresponds to your device in the listing.
+# #     Type or paste the full file name that corresponds to the file name that you found in the deCONZ container logs in step 1 (or, select a different filename, but you should have a good reason for doing this).
+# #     If the device/path and file name look OK, type Y to start flashing!
 
-#     Restart your deCONZ container (docker start [container name] or docker-compose up).
-}
+# #     Restart your deCONZ container (docker start [container name] or docker-compose up).
+# }
 run() {
     args=""
     if [[ -f /etc/localtime ]]; then
