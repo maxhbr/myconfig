13484f78737b24234b86d0b00ed46d1a00d588ec
Maximilian Huber <gh@maxhbr.de>
2020-01-12 20:36:18 +0100

    minor fix
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/desktop/xmonad/myXmonad/fast-test-in-xephyr.sh
+++ modules/desktop/xmonad/myXmonad/fast-test-in-xephyr.sh
@@ -81,7 +81,7 @@ EOF
 
 DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-result="$(nix-build '<nixpkgs>' -I nixpkgs=$DIR/../nix/nixpkgs --no-out-link --keep-failed -A myconfig.my-xmonad)"
+result="$(nix-build '<nixpkgs>' -I nixpkgs=$DIR/../nixpkgs --no-out-link --keep-failed -A myconfig.my-xmonad)"
 
 echo -e "\n\n\nresult is $result\n"
 
