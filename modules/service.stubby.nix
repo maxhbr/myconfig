64ab9cc88c6f9d0523163d5a493532b8db0a42ec
Maximilian Huber <gh@maxhbr.de>
2020-01-15 13:22:06 +0100

    wip: is it broken??
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ modules/service.stubby.nix
@@ -0,0 +1,31 @@
+# Copyright 2017-2019 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+{ ... }:
+{
+  services.stubby = {
+    enable = true;
+    upstreamServers = ''
+      - address_data: 1.1.1.1
+        tls_port: 853
+        tls_auth_name: "cloudflare-dns.com"
+      - address_data: 1.0.0.1
+        tls_port: 853
+        tls_auth_name: "cloudflare-dns.com"
+    '';
+  };
+
+  environment.etc = {
+    "resolv.conf" = {
+      text = ''
+        options edns0
+        nameserver 127.0.0.1
+      '';
+      mode = "444";
+    };
+  };
+
+  networking = {
+    networkmanager.dns = "none";
+    resolvconf.dnsExtensionMechanism = false;
+  };
+}
