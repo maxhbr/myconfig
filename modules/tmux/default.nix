66fc8084c588bf7e2f53262687721475a40a5b26
Maximilian Huber <gh@maxhbr.de>
2020-01-15 13:28:24 +0100

    copy tmuxconf from https://github.com/nrdxp/nixflk/tree/template/profiles/develop/tmux
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -1,16 +1,44 @@
 # Copyright 2017-2020 Maximilian Huber <oss@maximilian-huber.de>
 # SPDX-License-Identifier: MIT
-{ config, pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  inherit (builtins) readFile concatStringsSep;
 
-{
-  config = {
-    home-manager.users.mhuber = {
-      home.packages = with pkgs; [
-        tmux
-      ];
-      home.file = {
-        ".tmux.conf".source = ./tmux.conf;
-      };
-    };
+  inherit (lib) removePrefix;
+
+  pluginConf = plugins:
+    concatStringsSep "\n\n" (map (plugin:
+      let name = removePrefix "tmuxplugin-" plugin.name;
+      in "run-shell ${plugin}/share/tmux-plugins/${name}/${name}.tmux")
+      plugins);
+
+  plugins = with pkgs.tmuxPlugins; [
+    copycat
+    open
+    resurrect
+    yank
+    vim-tmux-navigator
+  ];
+in {
+  environment.shellAliases = {
+    tx = "tmux new-session -A -s $USER";
+    tmx = "tmux attach-session || tmux"
+  };
+  # TODO: ntmx with keybindings
+
+  programs.tmux = {
+    enable = true;
+    aggressiveResize = true;
+    escapeTime = 10;
+    historyLimit = 5000;
+    keyMode = "vi";
+    shortcut = "a";
+    terminal = "tmux-256color";
+    baseIndex = 1;
+    extraTmuxConf = ''
+      ${readFile ./tmuxline.conf}
+      ${readFile ./tmux.conf}
+      ${pluginConf plugins}
+    '';
   };
 }
