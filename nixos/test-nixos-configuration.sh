1ab35c9017a091bee6fd9cffacfe772f8a8bf71d
Maximilian Huber <gh@maxhbr.de>
2020-02-20 21:10:29 +0100

    maybe fix ci
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- nixos/test-nixos-configuration.sh
+++ nixos/test-nixos-configuration.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # see also: https://nixos.mayflower.consulting/blog/2018/09/11/custom-images/
 
-# $ $0 [--no-bc] [--ci] [base] [--check]
+# $ $0 [--no-bc] [--ci [base]] [--check]
 
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../common.sh"
 
@@ -10,7 +10,7 @@ args=""
 
 if [[ "$1" == "--no-bc" ]]; then
     shift
-     args="$args --option binary-caches ''"
+    args="$args --option binary-caches ''"
 fi
 
 if [[ "$1" == "--ci" ]]; then
@@ -18,9 +18,9 @@ if [[ "$1" == "--ci" ]]; then
     if [[ ! -f "$myconfigDir/hostname" ]]; then
         cat <<EOF > "$myconfigDir/imports/dummy-hardware-configuration.nix"
 {...}: {
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4";};
-  fileSystems."/boot" ={ device = "/dev/sda1"; fsType = "vfat";};
-  boot.loader.grub.devices = [ "/dev/sda1" ];
+  fileSystems."/" = { device = "/dev/sdXX"; fsType = "ext4"; };
+  fileSystems."/boot" = { device = "/dev/sdXY"; fsType = "vfat"; };
+  boot.loader.grub.devices = [ "/dev/sdXY" ];
 }
 EOF
 
@@ -32,10 +32,16 @@ EOF
         else
             echo -n "base" | tee "$myconfigDir/hostname"
         fi
+    else
+        echo "this system is already provisioned, can't run this test"
+        exit 1
     fi
 fi
 
-args="$args $@"
+if [[ "$1" == "--check" ]]; then
+    shift
+    args="$args --check"
+fi
 
 set -x
 
@@ -44,5 +50,6 @@ time \
               $NIX_PATH_ARGS \
               $args \
               -A system \
+              --no-out-link \
               --show-trace --keep-failed \
               --arg configuration "$nixosConfig"
