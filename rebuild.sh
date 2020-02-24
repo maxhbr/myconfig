22dac24d1ca52eaf3f3ca56cf5835b224e7acf57
Maximilian Huber <gh@maxhbr.de>
2020-02-24 18:54:08 +0100

    fix rebuild.sh
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- rebuild.sh
+++ rebuild.sh
@@ -331,7 +331,7 @@ update() {
             logH3 "update" "nixpkgs-unstable"
             ./nixos/lib/nixpkgs-unstable/update.sh
             logH3 "update" "emacs"
-            ./profiles/emacs/update.sh
+            ./nixos/base/modules/update.sh
             logH3 "update" "my-wallpapers"
             ./nixos/desktop/modules/common/my-wallpapers/update.sh
         fi
