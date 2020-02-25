6536d27cc4f2eadf7d25dce2d443d7c41a15f060
Maximilian Huber <gh@maxhbr.de>
2020-02-25 08:58:11 +0100

    improve setupXdgMime.sh script, but should start on #34
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- misc/post_install_hooks/setupXdgMime.sh
+++ misc/post_install_hooks/setupXdgMime.sh
@@ -8,7 +8,24 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 common="./common.sh"; until [ -f "$common" ]; do common="./.${common}"; done
 . "$common"
 
-logH1 "Setup xdg-mime"
+################################################################################
+setupFilesAndSymlinks() {
+    if [[ ! -f ~/.config/mimeapps.list ]]; then
+        mkdir -p ~/.config
+        touch ~/.config/mimeapps.list
+    fi
+
+    if [[ ! -L ~/.local/share/applications/mimeapps.list ]]; then
+        if [[ -f ~/.local/share/applications/mimeapps.list ]]; then
+            echo "remove old ~/.local/share/applications/mimeapps.list, content was:"
+            cat ~/.local/share/applications/mimeapps.list
+            rm ~/.local/share/applications/mimeapps.list
+        fi
+        mkdir -p ~/.local/share/applications
+        ln -s ~/.config/mimeapps.list ~/.local/share/applications/mimeapps.list
+    fi
+}
+
 setupXdgMime() {
     mimetype="$1"
     app="${2}.desktop"
@@ -21,6 +38,9 @@ setupXdgMime() {
     fi
 }
 
+################################################################################
+logH1 "Setup xdg-mime"
+setupFilesAndSymlinks
 setupXdgMime application/pdf mupdf
 setupXdgMime image/jpeg sxiv
 setupXdgMime image/png sxiv
