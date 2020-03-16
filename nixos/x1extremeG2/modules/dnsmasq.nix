3cb81af298ffe58441247e8834257df1c046d7d4
Maximilian Huber <gh@maxhbr.de>
2020-03-16 04:31:30 +0100

    add new host nuc
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ nixos/x1extremeG2/modules/dnsmasq.nix
@@ -0,0 +1,12 @@
+# Copyright 2019 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+{ pkgs, ... }:
+
+{
+  services.dnsmasq = {
+    enable = true;
+    extraConfig = ''
+conf-dir=/etc/dnsmasq.d
+'';
+  };
+}
