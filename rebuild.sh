33e4c9a02fba3da19bc9d7936cc5a445f4072257
Maximilian Huber <gh@maxhbr.de>
2020-03-18 09:42:44 +0100

    gc always if disk is relatively full
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- rebuild.sh
+++ rebuild.sh
@@ -338,7 +338,11 @@ cleanup() {
     logH2 "cleanup" "nixos and nix-env"
     pcent=$(echo $(df -h /  --output=pcent | tail -1 | cut -d'%' -f1))
     if [[ "$pcen" -gt 90 || "$((RANDOM%100))" -gt 90 ]]; then
-        ./nixos/gc.sh
+        if [[ "$pcen" -gt 95 ]]; then
+            ./nixos/gc.sh 15d
+        else
+            ./nixos/gc.sh
+        fi
     else
         echo "* $(tput bold)do not$(tput sgr0) gc ..."
     fi
