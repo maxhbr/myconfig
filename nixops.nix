0eb2052d8b2710f73a066ba5516c77db803a07d8
Maximilian Huber <gh@maxhbr.de>
2020-05-08 10:45:56 +0200

    refactor nixops
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- nixops.nix
+++ nixops.nix
@@ -1,7 +1,7 @@
 let
   secrets = import ./nixops-secrets.nix;
   hostFromConfig = hostName: addConfig:
-    { config, pkgs, ... }:
+    { config, pkgs, ... }@args:
     { config =
         { deployment.targetEnv = "none";
           assertions =
@@ -12,48 +12,51 @@ let
         } // secrets."${hostName}";
       imports =
         [ (./nixos/host- + hostName)
-          addConfig
+          (addConfig args)
         ];
     };
 in
 { network.description = "myconfig";
   x1extremeG2 = hostFromConfig "x1extremeG2"
-    { deployment.targetHost = "10.199.199.2";
-    };
+    ( {lib, ...}:
+      { deployment.targetHost = lib.mkDefault "10.199.199.2";
+      });
   workstation = hostFromConfig "workstation"
-    { deployment.targetHost = "10.199.199.5";
-      deployment.keys =
-        { wg-private = {
-            text = builtins.readFile ../wireguard-keys/workstation/private;
-            user = "root";
-            group = "root";
-            permissions = "0400";
+    ( {lib, ...}:
+      { deployment.targetHost =  lib.mkDefault "10.199.199.5";
+        deployment.keys =
+          { wg-private =
+              { text = builtins.readFile ../wireguard-keys/workstation/private;
+                user = "root";
+                group = "root";
+                permissions = "0400";
+              };
+            wg-public =
+              { text = builtins.readFile ../wireguard-keys/workstation/public;
+                user = "root";
+                group = "root";
+                permissions = "0400";
+              };
           };
-          wg-public = {
-            text = builtins.readFile ../wireguard-keys/workstation/public;
-            user = "root";
-            group = "root";
-            permissions = "0400";
-          };
-       };
-    };
+      });
   vserver = hostFromConfig "vserver"
-    { deployment.targetHost = "10.199.199.1";
-      deployment.keys =
-        { wg-private = {
-            text = builtins.readFile ../wireguard-keys/vserver/private;
-            user = "root";
-            group = "root";
-            permissions = "0400";
+    ( {lib, ...}:
+      { deployment.targetHost = lib.mkDefault "10.199.199.1";
+        deployment.keys =
+          { wg-private =
+              { text = builtins.readFile ../wireguard-keys/vserver/private;
+                user = "root";
+                group = "root";
+                permissions = "0400";
+              };
+            wg-public =
+              { text = builtins.readFile ../wireguard-keys/vserver/public;
+                user = "root";
+                group = "root";
+                permissions = "0400";
+              };
           };
-          wg-public = {
-            text = builtins.readFile ../wireguard-keys/vserver/public;
-            user = "root";
-            group = "root";
-            permissions = "0400";
-          };
-       };
-    };
-  T470p = hostFromConfig "T470p" {};
-  T470s = hostFromConfig "T470s" {};
+      });
+  T470p = hostFromConfig "T470p" ({...}: {});
+  T470s = hostFromConfig "T470s" ({...}: {});
 }
