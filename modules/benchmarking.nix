6ebc8a80227a096cc1485b4c6c17e2df2f9b1acd
Maximilian Huber <gh@maxhbr.de>
2020-05-19 15:20:20 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/benchmarking.nix
+++ modules/benchmarking.nix
@@ -2,10 +2,20 @@
 let
   blender-benchmark-launcher-cli = pkgs.callPackage ../pkgs/blender-benchmark-launcher-cli {};
   blender-benchmark-launcher = pkgs.callPackage ../pkgs/blender-benchmark-launcher {};
+  mybench = with pkgs; writeShellScriptBin "mybench" ''
+    benchDir="$HOME/Desktop/mybench/$(hostname)"
+    mkdir -p $benchDir
+    ${openssl}/bin/openssl speed | tee $benchDir/sslspeed
+    ${geekbench}/bin/geekbench5 --save $benchDir/geekbench5
+  '';
 in {
   config = {
     home-manager.users.mhuber = {
-      home.packages = [ ];
+      home.packages =
+        with pkgs;
+        [ geekbench
+          mybench
+        ];
     };
   };
 }
