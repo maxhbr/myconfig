8ada544c0b9648e852f087adbd6dba98cf32b226
Maximilian Huber <gh@maxhbr.de>
2020-06-12 13:09:52 +0200

    work on nas config
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ hosts/nas/4x500-hdds.raid.nix
@@ -0,0 +1,34 @@
+{...}:
+# ########################################################################
+# /mnt/data
+# see:
+# - https://github.com/NixOS/nixpkgs/issues/75540
+# - https://gist.github.com/MaxXor/ba1665f47d56c24018a943bb114640d7
+{ systemd.packages = [ pkgs.systemd-cryptsetup-generator ];
+  environment.etc.crypttab.text = ''
+    data0 UUID=f435b742-75da-4a0b-894f-06f329afebb8 /etc/cryptkey luks,noearly
+    data1 UUID=327a652d-79b0-4740-950c-684d5b56e66d /etc/cryptkey luks,noearly
+    data2 UUID=da356132-803e-439b-886d-91d70048b7f9 /etc/cryptkey luks,noearly
+    data3 UUID=c5e77337-21ad-4338-987a-710201dd5636 /etc/cryptkey luks,noearly
+  '';
+  fileSystems."/mnt/data0" =
+    { device = "/dev/mapper/data0";
+      fsType = "btrfs";
+      options = [ "defaults" "noatime" "compress=zstd" "subvol=@sub" "nofail" "x-systemd.requires-mounts-for=/mnt/.data3" ];
+    };
+  fileSystems."/mnt/.data1" =
+    { device = "/dev/mapper/data1";
+      fsType = "btrfs";
+      options = [ "defaults" "noatime" "compress=zstd" "subvol=@sub" "nofail" ];
+    };
+  fileSystems."/mnt/.data2" =
+    { device = "/dev/mapper/data2";
+      fsType = "btrfs";
+      options = [ "defaults" "noatime" "compress=zstd" "subvol=@sub" "nofail" "x-systemd.requires-mounts-for=/mnt/.data1" ];
+    };
+  fileSystems."/mnt/.data3" =
+    { device = "/dev/mapper/data3";
+      fsType = "btrfs";
+      options = [ "defaults" "noatime" "compress=zstd" "subvol=@sub" "nofail" "x-systemd.requires-mounts-for=/mnt/.data2" ];
+    };
+}
