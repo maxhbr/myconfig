498a05053ef975f59a8212bb58dc9e285488d8d5
Maximilian Huber <gh@maxhbr.de>
2020-06-19 18:40:48 +0200

    add nixops for x1extremeG2
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ hosts/x1extremeG2/nixops.nix
@@ -0,0 +1,27 @@
+with (import ../lib.nix);
+mkHostNixops "x1extremeG2"
+( {lib, ...}:
+  { config =
+      { deployment.targetHost = lib.mkDefault "10.199.199.2";
+        environment.shellAliases =
+          { upg-workstation = "upg-fast --target workstation";
+            upg-workstation-reboot = "upg-fast --target workstation --reboot";
+            upg-vserver = "upg-fast --target vserver";
+            upg-vserver-reboot = "upg-fast --target vserver --reboot";
+          };
+      };
+    imports =
+      [ (deployWireguardKeys "x1extremeG2")
+        (deploySSHUserKeys "x1extremeG2" "rsa")
+        (deploySSHUserKeys "x1extremeG2" "ed25519")
+        (deploySSHUserKeys "x1extremeG2" "dsa")
+        (deploySSHUserKeys "x1extremeG2" "ecdsa")
+        (setupNixServe
+          [ (getSecret "workstation" "ssh/id_rsa.pub")
+            (getSecret "vserver" "ssh/id_rsa.pub")
+          ])
+        (setupBuildSlave (getSecret "workstation" "ip") 2 (getSecret "x1extremeG2" "ssh/id_ed25519") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtqOcfT93S4gCROVvhTxB3Imp4bnbrtzQRnF9oRLTDs")
+        # (setupBuildSlave workstationIp 0.5 (getSecret "x1extremeG2" "ssh/id_ed25519") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtqOcfT93S4gCROVvhTxB3Imp4bnbrtzQRnF9oRLTDs")
+      ];
+    }
+)
