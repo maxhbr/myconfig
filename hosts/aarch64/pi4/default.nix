1fa06e854654112daee8f820735e9bc3277255de
Maximilian Huber <gh@maxhbr.de>
2020-07-15 14:16:37 +0200

    fix make-ext4-fs.nix issue
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/aarch64/pi4/default.nix
+++ hosts/aarch64/pi4/default.nix
@@ -14,5 +14,14 @@
 
   # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low
   # on space.
-  sdImage.compressImage = false;
+  sdImage =
+    { compressImage = false;
+      firmwareSize = lib.mkForce 1024;
+
+      # the make-ext4-fs.nix fails, if no files were writen in ./files
+      # See: https://github.com/NixOS/nixpkgs/pull/93175
+      populateRootCommands = "touch files/touched";
+    };
+
+  services.mingetty.autologinUser = lib.mkForce null;
 }
