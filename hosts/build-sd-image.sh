290107effc9372ec328acf263d8f5a245e643357
Maximilian Huber <gh@maxhbr.de>
2020-07-16 17:28:34 +0200

    allow to build default sd image
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/build-sd-image.sh
+++ hosts/build-sd-image.sh
@@ -10,16 +10,22 @@ common="./common.sh"; until [ -f "$common" ]; do common="./.${common}"; done
 build() (
     local hostname="$1"
     local nixpkgsDir="$nixpkgs"
+
     set -x
-    time nix-build \
-         -A config.system.build.sdImage \
-         --option system aarch64-linux \
-         -I nixos-config="${myconfigDir}/hosts/${hostname}/default.nix" \
-         -I nixpkgs="$nixpkgsDir" \
-         --no-out-link \
-         --show-trace --keep-failed \
-         --option sandbox false \
-         "${nixpkgsDir}/nixos/default.nix"
+    if [[ -z "$hostname" ]]; then
+        local aarchConfig="${nixpkgsDir}/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix"
+    else
+        local aarchConfig="${myconfigDir}/hosts/${hostname}/default.nix"
+    fi
+        time nix-build \
+             -A config.system.build.sdImage \
+             --option system aarch64-linux \
+             -I nixos-config="${aarchConfig}" \
+             -I nixpkgs="$nixpkgsDir" \
+             --no-out-link \
+             --show-trace --keep-failed \
+             --option sandbox false \
+             "${nixpkgsDir}/nixos/default.nix"
 )
 
 buildAndCopy() {
@@ -27,7 +33,11 @@ buildAndCopy() {
     drv=$(build "$hostname")
     out=("$drv/sd-image/"*".img")
     du -h "$out"
-    local outDir="$myconfigDir/__out/$hostname"
+    if [[ -z "$hostname" ]]; then
+        local outDir="$myconfigDir/__out/aarch64"
+    else
+        local outDir="$myconfigDir/__out/$hostname"
+    fi
     install -D -m 644 -v "$out" -t "$outDir"
     nix-store --delete "$drv"
 
