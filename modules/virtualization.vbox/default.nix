3e8918ddb3db31b1438900722eece916bcd398e0
Maximilian Huber <gh@maxhbr.de>
2020-07-29 10:50:37 +0200

    vargant is not working
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/virtualization.vbox/default.nix
+++ modules/virtualization.vbox/default.nix
@@ -1,29 +1,9 @@
 # Copyright 2019-2020 Maximilian Huber <oss@maximilian-huber.de>
 # SPDX-License-Identifier: MIT
-{ pkgs, ... }:
+{ ... }:
 
-{
-  config = {
-    home-manager.users.mhuber = {
-      home.packages = with pkgs; [
-        vagrant
-      ];
-      home.file = {
-        ".vagrant.d/Vagrantfile".source = ./vagrant.d/Vagrantfile;
-      };
+{ # imports = [ ./vagrant.nix ];
+  config =
+    { virtualisation.virtualbox.host.enable = true;
     };
-    virtualisation.virtualbox.host.enable = true;
-    environment = {
-      shellAliases = {
-        vup = "vagrant up";
-        vstart = "vagrant up";
-        vreload = "vagrant reload";
-        vhalt = "vagrant halt";
-        vstop = "vagrant halt";
-        vdown = "vagrant halt";
-        vssh = "vagrant ssh";
-        vdestroy = "vagrant destroy";
-      };
-    };
-  };
 }
---
+++ modules/virtualization.vbox/vagrant.nix
@@ -0,0 +1,28 @@
+# Copyright 2019-2020 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+{ pkgs, ... }:
+
+{
+  config = {
+    home-manager.users.mhuber = {
+      home.packages = with pkgs; [vagrant];
+      home.file = {
+        ".vagrant.d/Vagrantfile".source = ./vagrant.d/Vagrantfile;
+      };
+    };
+    environment = {
+      shellAliases = {
+        vup = "vagrant up";
+        vstart = "vagrant up";
+        vreload = "vagrant reload";
+        vhalt = "vagrant halt";
+        vstop = "vagrant halt";
+        vdown = "vagrant halt";
+        vssh = "vagrant ssh";
+        vdestroy = "vagrant destroy";
+      };
+    };
+
+    virtualisation.virtualbox.host.enable = true;
+  };
+}
