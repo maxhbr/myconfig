dd08e5af03860184ebf2b8cbcb40ed9e5288bdab
Maximilian Huber <gh@maxhbr.de>
2020-08-05 07:57:20 +0200

    work on iso
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/iso.nix
+++ hosts/iso.nix
@@ -3,47 +3,49 @@
 , hostConfig ? "roles/dev.nix" }:
 let
   nixpkgs = ../nixpkgs;
-  myisoconfig = { lib, ... }: {
-    imports = [
-      "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-      "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-      (../. + "/${hostConfig}")
-      ../modules/service.openssh.nix
-    ];
+  evalNixos = configuration:
+    import "${nixpkgs}/nixos"
+      { inherit system configuration;
+      };
+  myisoconfig = { lib, ... }:
+    { imports =
+        [ "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+          (../. + "/${hostConfig}")
+          ../modules/service.openssh.nix
+        ];
 
-    config = {
-      networking.hostName = "myconfig";
-      networking.wireless.enable = false;
+      config =
+        { networking.hostName = "myconfig";
+          networking.wireless.enable = false;
 
-      services.xserver.displayManager.autoLogin =
-        { enable = true;
-          user = "mhuber";
+          services.xserver.displayManager.autoLogin =
+            { enable = true;
+              user = "mhuber";
+            };
+
+          # OpenSSH is forced to have an empty `wantedBy` on the installer system[1], this won't allow it
+          # to be automatically started. Override it with the normal value.
+          # [1] https://github.com/NixOS/nixpkgs/blob/9e5aa25/nixos/modules/profiles/installation-device.nix#L76
+          systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
+
+          # add myconfig to iso
+          isoImage =
+            { contents =
+                [ # { source = ./.; target = "myconfig/nixos"; }
+                  # { source = ../misc; target = "myconfig/misc"; }
+                  # { source = ../nixpkgs; target = "myconfig/nixpkgs"; }
+                  # { source = ../common.sh; target = "myconfig/common.sh"; }
+                  # { source = ../rebuild.sh; target = "myconfig/rebuild.sh"; }
+                  # { source = ../README.org; target = "myconfig/README.org"; }
+                  { source = ../LICENSE; target = "myconfig/LICENSE"; }
+                ];
+            };
         };
-
-      # OpenSSH is forced to have an empty `wantedBy` on the installer system[1], this won't allow it
-      # to be automatically started. Override it with the normal value.
-      # [1] https://github.com/NixOS/nixpkgs/blob/9e5aa25/nixos/modules/profiles/installation-device.nix#L76
-      systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
-
-      # add myconfig to iso
-      isoImage.contents = [
-        # { source = ./.; target = "myconfig/nixos"; }
-        # { source = ../misc; target = "myconfig/misc"; }
-        # { source = ../nixpkgs; target = "myconfig/nixpkgs"; }
-        # { source = ../common.sh; target = "myconfig/common.sh"; }
-        # { source = ../rebuild.sh; target = "myconfig/rebuild.sh"; }
-        # { source = ../README.org; target = "myconfig/README.org"; }
-        { source = ../LICENSE; target = "myconfig/LICENSE"; }
-      ];
     };
-  };
 
-  evalNixos = configuration: import "${nixpkgs}/nixos" {
-    inherit system configuration;
-  };
-
-in {
-  iso = (evalNixos myisoconfig).config.system.build.isoImage;
+in
+{ iso = (evalNixos myisoconfig).config.system.build.isoImage;
 }
 
 
