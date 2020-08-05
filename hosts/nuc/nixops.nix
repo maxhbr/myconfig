eb572f0c35a12a89b6bf0e4f4bdcf88887ed498d
Maximilian Huber <gh@maxhbr.de>
2020-08-05 16:12:14 +0200

    update after rebuild.sh

--- hosts/nuc/nixops.nix
+++ hosts/nuc/nixops.nix
@@ -2,7 +2,7 @@ with (import ../lib.nix);
 mkHostNixops "nuc"
 ( {lib, ...}:
   { config =
-      {
+      { deployment.targetHost = lib.mkDefault "10.199.199.9";
       };
     imports =
       [ (fixIp "nuc" "enp2s0")
