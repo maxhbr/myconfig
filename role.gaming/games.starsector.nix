90368c118f6bdbd970f0e72f2a5c297932887b57
Maximilian Huber <gh@maxhbr.de>
2020-10-03 21:20:41 +0200

    activate nativeComp for emacs
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- role.gaming/games.starsector.nix
+++ role.gaming/games.starsector.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  config = {
+  config = lib.mkIf (builtins.pathExists
+    ../../pkgs/starsector/starsector_linux-0.9.1a-RC8.zip) {
     home-manager.users.mhuber =
       let starsector = pkgs.unstable.callPackage ../pkgs/starsector { };
       in { home.packages = [ starsector ]; };
