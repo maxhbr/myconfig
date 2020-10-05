44291a973926177a395d4c67da2a422bbfc424ac
Maximilian Huber <gh@maxhbr.de>
2020-10-05 07:23:43 +0200

    fix starsector path
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- role.gaming/games.starsector.nix
+++ role.gaming/games.starsector.nix
@@ -2,7 +2,7 @@
 
 {
   config = lib.mkIf (builtins.pathExists
-    ../../pkgs/starsector/starsector_linux-0.9.1a-RC8.zip) {
+    ../pkgs/starsector/starsector_linux-0.9.1a-RC8.zip) {
       home-manager.users.mhuber =
         let starsector = pkgs.unstable.callPackage ../pkgs/starsector { };
         in { home.packages = [ starsector ]; };
