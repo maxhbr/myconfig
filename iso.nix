d8f501fc371b36fd0410c3528c7e7e8eff38ef93
Maximilian Huber <gh@maxhbr.de>
2020-11-03 17:18:35 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- iso.nix
+++ iso.nix
@@ -58,7 +58,7 @@ let
         # add myconfig to iso
         isoImage = {
           contents = [{
-             source = pkgs.nix-gitignore.gitignoreRecursiveSource [] ./.;
+             source = pkgs.nix-gitignore.gitignoreSource [] ./.;
             target = "myconfig";
           }];
           isoBaseName = "nixos-myconfig";
