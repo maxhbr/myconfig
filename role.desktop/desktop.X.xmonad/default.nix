a741a10d72fbdeacd38983e7653667c5f4ff4e8a
Maximilian Huber <gh@maxhbr.de>
2020-11-14 19:13:13 +0100

    restart picom by autorandr
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- role.desktop/desktop.X.xmonad/default.nix
+++ role.desktop/desktop.X.xmonad/default.nix
@@ -17,6 +17,7 @@ in {
 
   config = {
     home-manager.users."${user}" = {
+      imports = [ ./picom.hm.nix ];
       home.packages = with pkgs; [
         my-xmonad
         my-xmobar
@@ -40,39 +41,6 @@ in {
             fi
           '';
         };
-      };
-      services.picom = let
-        excludes = [
-          "window_type *= 'menu'"
-          "class_g = 'firefox' && argb"
-          "name ~= 'Zoom Meeting'"
-          "class_g = 'zoom'"
-          # "name ~= 'Firefox$'"
-          # "focused = 1"
-          # # "menu        = { shadow = false; };"
-          # "dropdown_menu = { shadow = false; };"
-          # "popup_menu    = { shadow = false; };"
-          # "utility       = { shadow = false; };"
-        ];
-      in {
-        enable = true;
-        blur = true;
-        blurExclude = excludes;
-        fade = true;
-        fadeExclude = excludes;
-        shadow = true;
-        shadowExclude = excludes;
-        vSync = true;
-        inactiveDim = "0.2";
-        menuOpacity = "0.8";
-        extraOptions = ''
-          unredir-if-possible = true;
-          dbe = true;
-          inactive-opacity-override = false;
-          # https://www.reddit.com/r/xmonad/comments/ih2scc/picom_opacity_not_working_in_xmonad/
-          mark-ovredir-focused = false;
-          use-ewmh-active-win = true;
-        '';
       };
     };
     environment.variables = {
--- role.desktop/st/default.nix
+++ role.desktop/st/default.nix
@@ -15,6 +15,14 @@
               ];
             })).override { conf = builtins.readFile ./st-config.h; };
           in {
+            st-notabbed = stpre;
+            st-notmux = prev.writeShellScriptBin "st-notmux" ''
+              if [ $# -eq 0 ]; then
+                exec ${final.tabbed}/bin/tabbed -d -c -r 2 ${stpre}/bin/st
+              else
+                exec ${stpre}/bin/st "$@"
+              fi
+            '';
             st = prev.writeShellScriptBin "st" ''
               if [ $# -eq 0 ]; then
                 exec ${final.tabbed}/bin/tabbed -d -c -r 2 ${stpre}/bin/st -w ’’ -e ${final.tmux}/bin/tmux
@@ -22,10 +30,20 @@
                 exec ${stpre}/bin/st "$@"
               fi
             '';
-            st-notabbed = stpre;
           })
       ];
     };
-    environment.systemPackages = with pkgs; [ tabbed st ];
+    environment.systemPackages = with pkgs; [
+      tabbed st st-notmux
+      (writeShellScriptBin "st-reattach" ''
+${tmux}/bin/tmux ls |
+    ${gnugrep}/bin/grep -v '(attached)' |
+    cut -f 1 -d ":" |
+    while read SESSION; do
+        (set -x;
+         ${st-notabbed}/bin/st -e ${tmux}/bin/tmux attach -t "$SESSION" & disown)
+    done
+'')
+    ];
   };
 }
