cafcc38581a392be8b67f2b6d4ec06805b864ce7
Maximilian Huber <gh@maxhbr.de>
2021-01-21 13:29:44 +0100

    update after rebuild.sh

--- pkgs/scanoss-scanner/default.nix
+++ pkgs/scanoss-scanner/default.nix
@@ -2,55 +2,55 @@
 , dpkg, makeWrapper, glibc, gcc-unwrapped, openssl, jq, ncurses }:
 let
   scanoss-dir = pkgs.writeShellScriptBin "scanoss-dir" ''
-    set -euo pipefail
+set -euo pipefail
 
-    run() {
-        local file="$1"
+run() {
+    local file="$1"
 
-        for counter in {1..9}; do
-            if [[ $counter -eq 1 ]]; then
-                (>&2 echo "[$counter] $file")
-            else
-                (>&2 echo "$(${ncurses}/bin/tput bold)$(${ncurses}/bin/tput setaf 1)[$counter] $file$(${ncurses}/bin/tput sgr0)")
-                sleep $(( $counter / 3 ))
-            fi
-            local result=$(scanoss-scanner "$file")
-
-            if ${jq}/bin/jq -e . >/dev/null 2>&1 <<<"$result"; then
-                echo "$result"
-                return
-            fi
-        done
-        (>&2 echo "... failed")
-        cat <<EOF
-    {
-      "$file": "...failed"
-    }
-    EOF
-    }
-
-    main() {
-        local workdir="$(readlink -f "$1")"
-        if [[ ! -d "$workdir" ]]; then
-            echo "the folder workdir=$workdir does not exist"
-            exit 1
+    for counter in {1..9}; do
+        if [[ $counter -eq 1 ]]; then
+            (>&2 echo "[$counter] $file")
+        else
+            (>&2 echo "$(${ncurses}/bin/tput bold)$(${ncurses}/bin/tput setaf 1)[$counter] $file$(${ncurses}/bin/tput sgr0)")
+            sleep $(( $counter / 3 ))
         fi
+        local result=$(scanoss-scanner "$file")
 
-        (cd "$workdir"
-        find . -type f \
-            -not -empty \
-            -not -path '*/\.git/*' \
-            -not -path '*/\.svn/*' |
-            while read file; do
-                run "$file"
-            done) |
-            tee "''${workdir}_sca.raw.json" |
-            ${jq}/bin/jq -n '[inputs] |
-            add' > "''${workdir}_sca.json"
-    }
+        if ${jq}/bin/jq -e . >/dev/null 2>&1 <<<"$result"; then
+            echo "$result"
+            return
+        fi
+    done
+    (>&2 echo "... failed")
+    cat <<EOF
+{
+  "$file": []
+}
+EOF
+}
 
-    main "$@"
-    times
+main() {
+    local workdir="$(readlink -f "$1")"
+    if [[ ! -d "$workdir" ]]; then
+        echo "the folder workdir=$workdir does not exist"
+        exit 1
+    fi
+
+    (cd "$workdir"
+    find . -type f \
+        -not -empty \
+        -not -path '*/\.git/*' \
+        -not -path '*/\.svn/*' |
+        while read file; do
+            run "$file"
+        done) |
+        sed '/^[[:space:]]*$/d' |
+        tee "''${workdir}_sca.raw.json" |
+        ${jq}/bin/jq -n '[inputs] | add' > "''${workdir}_sca.json"
+}
+
+main "$@"
+times
   '';
 in stdenv.mkDerivation rec {
   version = "1.01";
