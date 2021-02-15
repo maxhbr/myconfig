f894b65d2a04117ad072afa06321ad332389b6d9
Maximilian Huber <gh@maxhbr.de>
2021-02-15 11:24:51 +0100

    minor change
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/dev/programs.license-compliance-toolbox.nix
+++ modules/dev/programs.license-compliance-toolbox.nix
@@ -4,9 +4,6 @@ in {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       (import ../../pkgs/license-compliance-toolbox).packages.x86_64-linux.license-compliance-toolbox
-      # (pkgs.callPackage ../../pkgs/oss-review-toolkit-ort { })
-      # (pkgs.callPackage ../../pkgs/license-compliance-toolbox/scancode-workbench-git.nix { })
-      # (pkgs.callPackage ../../pkgs/scanoss-scanner { })
     ];
   };
 }
