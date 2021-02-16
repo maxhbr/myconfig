d95c6f541f12fec0f5fd9ef9e39a262d7b2dd686
Maximilian Huber <gh@maxhbr.de>
2021-02-16 17:22:46 +0100

    refactor script
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- iso/build-iso-image.sh
+++ iso/build-iso-image.sh
@@ -9,7 +9,7 @@ common="./common.sh"; until [ -f "$common" ]; do common="./.${common}"; done
 
 help() {
     cat <<EOF
-$@ [--push-to-cachix] path/to/config.nix [--dry-run] [arguments [..]]
+$0 [--push-to-cachix] path/to/config.nix [--dry-run] [arguments [..]]
 EOF
 }
 
@@ -26,6 +26,26 @@ getNamePrefixFromConfig() {
     else
         getNamePrefixFromConfig "$(dirname "$config")"
     fi
+}
+
+writeScripts() {
+    local outFile="$1"
+    local outDir
+    outDir="$(dirname "$outFile")"
+    cat <<EOF | tee "$outDir/dd.sh"
+#!/usr/bin/env bash
+set -ex
+sdX="\$1"
+sudo dd if="$outFile" of="\$sdX" bs=4M conv=sync
+EOF
+    chmod +x "$outDir/dd.sh"
+
+    cat <<EOF | tee "$outDir/run-qemu.sh"
+#!/usr/bin/env bash
+set -ex
+qemu-kvm -boot d -cdrom "$outFile" -m 32000 -cpu host -smp 6
+EOF
+    chmod +x "$outDir/run-qemu.sh"
 }
 
 build() {
@@ -56,6 +76,13 @@ build() {
           "$@")
 }
 
+getIsoFromDrv() {
+    local drv="$1"
+    local outArr
+    outArr=("$drv/iso/nixos-myconfig"*".iso")
+    echo "${outArr[-1]}"
+}
+
 buildAndCopy() {
     local pushToCachix=false
     if [[ "$1" == "--push-to-cachix" ]]; then
@@ -63,45 +90,37 @@ buildAndCopy() {
         shift
     fi
 
+
     local drv
     drv=$(build "$@")
+    if [[ "$*" == *"--dry-run"* ]]; then
+        return 0
+    fi
+
 
     if [[ "$pushToCachix" == "true" ]]; then
         echo "$drv" | cachix push maxhbr
     fi
 
-    local outArr
-    outArr=("$drv/iso/nixos-myconfig"*".iso")
-    local out="${outArr[-1]}"
+
+    local out="$(getIsoFromDrv "$drv")"
     du -h "$out"
+
+
     local outDir
     outDir="$myconfigDir/__out/iso$(getNamePrefixFromConfig "$(readlink -f $1)")"
     install -D -m 644 -v "$out" -t "$outDir"
+    writeScripts "$outDir/$(basename "$out")"
+
+
     nix-store --delete "$drv" || {
         sleep 20
-        nix-store --delete "$drv" || echo "failed to\n\tnix-store --delete \"$drv\""
+        nix-store --delete "$drv" || printf "failed to\n\tnix-store --delete \"$drv\""
     }
-
-    cat <<EOF | tee "$outDir/dd.sh"
-#!/usr/bin/env bash
-set -ex
-sdX="\$1"
-sudo dd if="$outDir/$(basename "$out")" of="\$sdX" bs=4M conv=sync
-EOF
-    chmod +x "$outDir/dd.sh"
-
-    cat <<EOF | tee "$outDir/run-qemu.sh"
-#!/usr/bin/env bash
-set -ex
-qemu-kvm -boot d -cdrom "$outDir/$(basename "$out")" -m 32000 -cpu host -smp 6
-EOF
-    chmod +x "$outDir/run-qemu.sh"
 }
 
 if [[ "$1" == "--help" ]]; then
     help
-elif [[ "$2" == "--dry-run" ]]; then
-    build "$@"
 else
     buildAndCopy "$@"
 fi
