4678d4db4c97a2a24aa6688fea77d95d0a2ec555
Maximilian Huber <gh@maxhbr.de>
2021-02-16 21:26:52 +0100

    update after rebuild.sh

--- host.x1extremeG2/default.nix
+++ host.x1extremeG2/default.nix
@@ -20,6 +20,10 @@ in {
     ./mail
     ## fun
     ./smarthome.nix
+    {
+      # Fingerprint reader: login and unlock with fingerprint (if you add one with `fprintd-enroll`)
+      services.fprintd.enable = true;
+    }
   ] ++ (with (import ../lib.nix); [
     (setupAsWireguardClient "10.199.199.2")
     # (setupNasNFS "bilder")
