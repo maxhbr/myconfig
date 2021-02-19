9dd5cd181f1068cb84985eda358882d6772ef025
Maximilian Huber <gh@maxhbr.de>
2021-02-19 18:53:19 +0100

    alias for nix-flakes stuff
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/nixos.nix.nix
+++ modules/nixos.nix.nix
@@ -4,18 +4,24 @@
 let user = config.myconfig.user;
 in {
   imports = [
-    # { # support for nix-flakes
-    #   # see:
-    #   # - https://nixos.wiki/wiki/Flakes
-    #   # - https://www.tweag.io/blog/2020-05-25-flakes/
-    #   config = {
-    #     nix = {
-    #       package = pkgs.nixFlakes;
-    #       extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
-    #         "experimental-features = nix-command flakes";
-    #     };
-    #   };
-    # }
+    { # support for nix-flakes
+      # see:
+      # - https://nixos.wiki/wiki/Flakes
+      # - https://www.tweag.io/blog/2020-05-25-flakes/
+      config = {
+        nix = {
+          package = pkgs.nixFlakes;
+          extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes)
+            "experimental-features = nix-command"; #  flakes
+        };
+
+        home-manager.users."${user}" = {
+          programs.fish = {
+            shellAbbrs = { nix-flake = "nix --experimental-features 'nix-command flakes' flake"; };
+          };
+        };
+      };
+    }
     {
       #       nix-closure-size() {
       #   nix-store -q --size $(nix-store -qR $(readlink -e $1) ) | \
