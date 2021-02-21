2dabebb46ae969fab8c88df856d2829beaf7cbdd
Maximilian Huber <gh@maxhbr.de>
2021-02-21 19:18:10 +0100

    update freeplane
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- pkgs/freeplane/default.nix
+++ pkgs/freeplane/default.nix
@@ -1,15 +1,14 @@
 # from: https://github.com/NixOS/nixpkgs/pull/34752
-{ stdenv, lib, fetchurl, unzip, makeWrapper }:
+{ stdenv, lib, fetchurl, unzip, makeWrapper, jdk11 }:
 
 stdenv.mkDerivation rec {
-  version = "1.8.6";
-  name = "freeplane-${version}";
+  version = "1.8.11";
+  pname = "freeplane";
 
   src = fetchurl {
     url =
       "https://sourceforge.net/projects/freeplane/files/freeplane%20stable/freeplane_bin-${version}.zip/download";
-    sha512 =
-      "185d69rhyw4i6k2chidsqgbif9qikk39li4fvw68aad3fl60p0blr48l5hgrkcfp39nkxkp188g76srnqi19kgrj1had1g57ad16jcl";
+    sha1 = "1038fc4e3b5fa86643e6d49d225fd811936c95fa";
   };
 
   nativeBuildInputs = [ unzip makeWrapper ];
@@ -21,7 +20,8 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/{bin,share}
     cp -r freeplane-${version}/* $out/share
-    makeWrapper $out/share/freeplane.sh $out/bin/freeplane
+    makeWrapper $out/share/freeplane.sh $out/bin/freeplane \
+      --set JAVA_HOME "${jdk11}/lib/openjdk" \
   '';
 
   meta = with lib; {
