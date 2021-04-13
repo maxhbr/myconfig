6a2c6303f6ba389362a785af7c1625e60ad21100
Maximilian Huber <gh@maxhbr.de>
2021-04-13 21:00:58 +0200

    cleanup in jlink
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flakes/zephyrproject/jlink/default.nix
+++ flakes/zephyrproject/jlink/default.nix
@@ -13,14 +13,8 @@ stdenv.mkDerivation rec {
         url = "https://www.segger.com/downloads/jlink/JLink_Linux_V${version}_x86_64.rpm";
         sha256 = "06g1x25afhpczaz3xlzyrfa00456mfjn9f0n8nf166faczwkcaj9";
       }
-    # else if stdenv.system == "i686-linux" then
-    #   requireFile {
-    #     name = "JLink_Linux_V${version}_i386.rpm";
-    #     url = "https://www.segger.com/downloads/jlink/JLink_Linux_V${version}_i386.rpm";
-    #     sha256 = "c8e169163980165eecbeac90787d649ea3ea036140e9c6ac613bc339c30445df";
-    #   }
     else
-      abort "${name} requires i686-linux or x86_64 Linux";
+      abort "${name} requires x86_64 Linux";
 
   buildInputs = [ rpmextract ];
   phases = [ "unpackPhase" "installPhase" "fixupPhase" "distPhase" ];
@@ -31,7 +25,6 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "SEGGER J-Links are the most widely used line of debug probes available today";
-    longDescription = "TODO:";
     homepage = https://www.segger.com/downloads/jlink;
     license = licenses.unfree;
     platforms = platforms.linux;
