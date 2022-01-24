f5090a2c05d10a86eae34a0bd1838b05993973e2
Maximilian Huber <gh@maxhbr.de>
2022-01-24 16:28:20 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flakes/mykeylight/mykeylight.py
+++ flakes/mykeylight/mykeylight.py
@@ -3,15 +3,16 @@
 
 import asyncio
 import argparse
+import os
 
 from elgato import Elgato, State, Info
-
 
 parser = argparse.ArgumentParser(description='mykeylight.')
 parser.add_argument('--on', action=argparse.BooleanOptionalAction)
 parser.add_argument('--off', action=argparse.BooleanOptionalAction)
 
-ips = ["192.168.1.202", "192.168.1.231"]
+config = open(os.path.expanduser('~/.mykeylight'), 'r')
+ips = config.readlines()
 
 async def toggle():
     async with Elgato(ips[0]) as elgato:
