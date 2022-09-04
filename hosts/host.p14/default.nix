c7cfc21c5d66251584fc2a0ae9d7ccea94f827ac
Maximilian Huber <gh@maxhbr.de>
2022-09-04 21:24:26 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.p14/default.nix
+++ hosts/host.p14/default.nix
@@ -100,6 +100,6 @@ index 51dde5bfd..5dcd32cf9 100644
     # this value at the release version of the first install of this system.
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = lib.mkForce "21.05"; # Did you read the comment?
+    system.stateVersion = lib.mkForce "22.05"; # Did you read the comment?
   };
 }
