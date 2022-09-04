4fa7a08158ed51d2b2ec60b2e22f9b5241acb4a9
Maximilian Huber <gh@maxhbr.de>
2022-09-04 13:43:35 +0200

    some wireguard setup
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ hosts/host.vserver/service.wireguard-server/peers/p14.nix
@@ -0,0 +1,7 @@
+# Copyright 2019 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+
+{ # p14
+  publicKey = "xmCf9rjyZ9Q9RJzc68YzaKQzWCZvtIMhRHftNx8rnkE=";
+  allowedIPs = [ "10.199.199.11/32" ];
+}
