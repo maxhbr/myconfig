4985e9d3d997696c41a99386ed9d1c9f90c45be6
Maximilian Huber <gh@maxhbr.de>
2023-10-08 17:31:13 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flake.nixosModules.mydwl.nix
+++ flake.nixosModules.mydwl.nix
@@ -1,6 +1,6 @@
-inputs: { pkgs, config, lib, ... }:
-let
-  cfg = config.myconfig;
+inputs:
+{ pkgs, config, lib, ... }:
+let cfg = config.myconfig;
 in {
   options.myconfig = with lib; {
     desktop.wayland.dwl = { enable = mkEnableOption "dwl"; };
@@ -15,7 +15,7 @@ in {
           ${cfg.desktop.wayland.autostartCommands}
           pkill waybar ; ${config.programs.waybar.package}/bin/waybar > /tmp/dwl.''${XDG_VTNR}.''${USER}.waybar.log 2>&1 &disown
           ${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all
-          '';
+        '';
       };
       myconfig.desktop.wayland.greetdSettings = {
         dwl_session = {
