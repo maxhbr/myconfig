2bffad07f9b55a6de2dd581c58ef11a66d28c282
Maximilian Huber <gh@maxhbr.de>
2023-10-31 21:53:36 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/programs.taskwarrior.nix
+++ modules/programs.taskwarrior.nix
@@ -9,6 +9,14 @@
             labels = [ "ID" "Started" "Age" "Priority" "Project" "Due" "Description" ];
           };
           tasksh.autoclear=1;
+
+          uda.priority.label="Priority";
+          uda.priority.type="string";
+          uda.priority.values="H,M,,L";
+
+          urgency.uda.priority.H.coefficient=6.0;
+          urgency.uda.priority.M.coefficient=3.9;
+          urgency.uda.priority.L.coefficient=0.9;
         };
         colorTheme = "light-256";
         dataLocation = "~/task";
