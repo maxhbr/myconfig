9a1b9967e1490a2c32d36836ebd402df372873a3
Maximilian Huber <gh@maxhbr.de>
2024-02-06 14:46:48 +0100

    adopt to https://github.com/hyprwm/Hyprland/pull/4439
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- modules/myconfig.desktop.wayland.hyprland/default.nix
+++ modules/myconfig.desktop.wayland.hyprland/default.nix
@@ -18,68 +18,7 @@ in {
     (cfg.desktop.wayland.enable && cfg.desktop.wayland.hyprland.enable) {
       home-manager.sharedModules = [({config, ...}: let
         hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
-        hyprctl-workspace = pkgs.writeShellScriptBin "hyprctl-workspace" ''
-# SPDX-License-Identifier: CC0-1.0
-
-set -euo pipefail
-
-jq="${pkgs.jq}/bin/jq"
-hyprctl="${hyprctl}"
-
-get_number_of_monitors() {
-    $jq -r '.|length'
-}
-
-get_active_monitor() {
-    $jq -r '[.[]|select(.focused == true)][0].id'
-}
-
-get_passive_monitor_with_workspace() {
-    local workspace="$1"
-    $jq -r '[.[]|select(.focused == false and .activeWorkspace.id == '"$workspace"')][0].id'
-}
-
-switch_to_workspace() {
-    local workspace="$1"
-    if [[ $# -eq 2 ]]; then
-        local activemonitor="$2"
-        $hyprctl dispatch moveworkspacetomonitor "$workspace $activemonitor"
-    fi
-    $hyprctl dispatch workspace "$workspace"
-}
-
-swap_active_workspaces() {
-    local activemonitor="$1"
-    local passivemonitor="$2"
-    $hyprctl dispatch swapactiveworkspaces "$activemonitor" "$passivemonitor"
-}
-
-main() {
-    if [[ $# -ne 1 ]]; then
-        echo "Usage: $0 <workspace>" >&2
-        exit 1
-    fi
-    local workspace=$1
-    local monitors="$($hyprctl -j monitors)"
-
-    if [[ $(echo "$monitors" | get_number_of_monitors) -eq 1 ]]; then
-        switch_to_workspace "$workspace"
-    fi
-
-    activemonitor="$(echo "$monitors" | get_active_monitor)"
-    passivemonitor="$(echo "$monitors" | get_passive_monitor_with_workspace "$workspace")"
-
-    if [[ "$passivemonitor" == "null" ]]; then
-        switch_to_workspace "$workspace" "$activemonitor"
-    else
-        swap_active_workspaces "$activemonitor" "$passivemonitor"
-    fi
-}
-
-main "$@"
-'';
         hyprctl-scripts = with pkgs; [
-          hyprctl-workspace 
           (writeShellScriptBin "hyprctl-animations-off" "${hyprctl} keyword animations:enabled no")
           (writeShellScriptBin "hyprctl-animations-on"  "${hyprctl} keyword animations:enabled yes")
           (writeShellScriptBin "hyprctl-create-headless" ''
