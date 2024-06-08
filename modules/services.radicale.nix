1aa29a7a2b885f64fc114d810ba3850d3b6f62c4
Maximilian Huber <gh@maxhbr.de>
2024-06-08 17:38:41 +0200

    add simple radicale module
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ modules/services.radicale.nix
@@ -0,0 +1,21 @@
+# Copyright 2024 Maximilian Huber <oss@maximilian-huber.de>
+# SPDX-License-Identifier: MIT
+# https://nixos.wiki/wiki/Radicale
+{ pkgs, config, lib, myconfig, ... }:
+let
+  user = myconfig.user;
+in {
+  config = lib.mkIf config.services.radicale.enable {
+    services.radicale = {
+      settings = {
+        server.hosts = [ "0.0.0.0:5232" ];
+        auth = {
+          type = "htpasswd";
+          # htpasswd_filename = "/etc/radicale_users.htpasswd";
+          # htpasswd_encryption = "bcrypt";
+        };
+      };
+    };
+  };
+}
+
