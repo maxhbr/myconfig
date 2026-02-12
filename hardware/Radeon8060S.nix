fdf3038822e239570d8ce7f570a26510cf24c62a
Maximilian Huber <gh@maxhbr.de>
2026-02-12 17:06:05 +0100

    wip
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hardware/Radeon8060S.nix
+++ hardware/Radeon8060S.nix
@@ -12,13 +12,19 @@ let
         rocmSupport = true;
         # Force the compiler to target the RDNA 3.5 ISA
         rocmGpuTargets = [ "gfx1151" ];
+        extraCmakeFlags = [ 
+          "-DGGML_HIP_ROCWMMA=ON" 
+          "-DGGML_HIP_ROCWMMA_FATTN=ON" # Critical for long context performance
+        ];
       })
     ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/llama-cli \
         --set HSA_OVERRIDE_GFX_VERSION 11.5.1 \
-        --set GGML_HIP_VISIBLE_DEVICES 0
+        --set GGML_HIP_VISIBLE_DEVICES 0\
+        --set HSA_ENABLE_SDMA 0 \
+        --set HIP_FORCE_DEV_KERNARG 1
     '';
   };
 
