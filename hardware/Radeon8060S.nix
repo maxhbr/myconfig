b49cd5dd0941ab4fb4c185510b488a12240493b7
Maximilian Huber <gh@maxhbr.de>
2026-02-12 16:39:47 +0100

    gfx1151
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hardware/Radeon8060S.nix
+++ hardware/Radeon8060S.nix
@@ -5,15 +5,15 @@
   ...
 }:
 let
-  # Optimized llama-cpp for Strix Halo
-  llama-cpp-max = pkgs.llama-cpp.override {
-    rocmSupport = true;
-    # Force the compiler to target the RDNA 3.5 ISA
-    rocmGpuTargets = [ "gfx1151" ];
-  };
-  llama-cpp-optimized = pkgs.symlinkJoin {
+  llama-cpp-gfx1151 = pkgs.symlinkJoin {
     name = "llama-cpp-optimized";
-    paths = [ llama-cpp-max ];
+    paths = [ 
+      (pkgs.llama-cpp.override {
+        rocmSupport = true;
+        # Force the compiler to target the RDNA 3.5 ISA
+        rocmGpuTargets = [ "gfx1151" ];
+      })
+    ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/llama-cli \
@@ -21,12 +21,17 @@ let
         --set GGML_HIP_VISIBLE_DEVICES 0
     '';
   };
+
+  ollama-rocm-gfx1151 = pkg.ollama-rocm.override {
+    rocmGpuTargets = [ "gfx1151" ];
+  };
 in
 {
   config = {
     myconfig = {
       hardware.gpu.variant = "amd";
-      ai.inference-cpp.ollama-cpp.package = llama-cpp-optimized;
+      ai.inference-cpp.ollama-cpp.package = llama-cpp-gfx1151;
     };
+    services.ollama.package = lib.mkForce ollama-rocm-gfx1151;
   };
 }
