5db07565d27553145c345f3123d038fea8d92fc8
Maximilian Huber <gh@maxhbr.de>
2026-02-12 12:27:59 +0100

    gfx1151
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.thing/ai.thing.nix
+++ hosts/host.thing/ai.thing.nix
@@ -59,6 +59,22 @@ in
             }
           );
     })
+    ({
+      config = 
+        lib.mkIf (config.myconfig.hardware.gpu.variant = "amd") {
+          services.ollama = {
+            environmentVariables = {
+              HSA_OVERRIDE_GFX_VERSION = “11.5.1”;
+              HIP_VISIBLE_DEVICES = “1”;
+              OLLAMA_LLM_LIBRARY = “rocm”;
+              HCC_AMDGPU_TARGET = “gfx1151”;
+              HSA_ENABLE_SDMA = “1”;
+              OLLAMA_DEBUG = “1”;
+            };
+            rocmOverrideGfx = “11.5.1”;
+          };
+        };
+    })
   ];
 
   config = {
