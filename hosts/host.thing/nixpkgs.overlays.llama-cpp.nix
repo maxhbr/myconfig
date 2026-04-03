3f7a05c4fbe0cc3d1c3482fad20e02aee4981f7f
Maximilian Huber <gh@maxhbr.de>
2026-04-03 18:30:52 +0200

    gpg
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.thing/nixpkgs.overlays.llama-cpp.nix
+++ hosts/host.thing/nixpkgs.overlays.llama-cpp.nix
@@ -1,29 +1,39 @@
 # Copyright 2025 Maximilian Huber <oss@maximilian-huber.de>
 # SPDX-License-Identifier: MIT
 { lib, ... }:
+let
+  version = "8642";
+  hash = "sha256-CGoqQd4jdcVlttg1fFkUNW4v3Rxwfkj+TVlcHD59RhI=";
+  npmDepsHash = "sha256-DxgUDVr+kwtW55C4b89Pl+j3u2ILmACcQOvOBjKWAKQ=";
+in
 {
   nixpkgs.overlays = [
     (final: prev: {
-      llama-cpp = prev.llama-cpp.overrideAttrs (oldAttrs: rec {
-        version = "8642";
+      llama-cpp = prev.llama-cpp.overrideAttrs (oldAttrs: {
+        inherit version;
         src = prev.fetchFromGitHub {
           owner = "ggml-org";
           repo = "llama.cpp";
           tag = "b${version}";
-          hash = "sha256-CGoqQd4jdcVlttg1fFkUNW4v3Rxwfkj+TVlcHD59RhI=";
+          inherit hash;
         };
         postPatch = (oldAttrs.postPatch or "") + ''
           echo "7c7d6ce" > COMMIT
         '';
-        npmDepsHash = lib.fakeHash;
+        inherit npmDepsHash;
         npmDeps = prev.fetchNpmDeps {
           name = "llama-cpp-${version}-npm-deps";
-          inherit src;
+          src = prev.fetchFromGitHub {
+            owner = "ggml-org";
+            repo = "llama.cpp";
+            tag = "b${version}";
+            inherit hash;
+          };
           inherit (oldAttrs) patches;
           preBuild = ''
             pushd ${oldAttrs.npmRoot}
           '';
-          hash = lib.fakeHash;
+          hash = npmDepsHash;
         };
       });
     })
--- scripts/priv.template/mk_gpg_keypair.sh
+++ scripts/priv.template/mk_gpg_keypair.sh
@@ -2,20 +2,21 @@
 #! nix-shell -i bash -p gnupg git
 
 set -ex
-hostname="$1"
+hostname="${1:?Usage: $0 <hostname>}"
 
 gpgDir="hosts/host.${hostname}/secrets/gpg"
 if [[ ! -d "$gpgDir" ]]; then
     mkdir -p "$gpgDir"
     GNUPGHOME="$gpgDir" gpg --batch --gen-key <<EOF
 %no-protection
-Key-Type: ed25519
-Key-Usage: sign
-Subkey-Type: cv25519
-Subkey-Usage: encrypt
+Key-Type: eddsa
+Key-Curve: ed25519
+Subkey-Type: ecdh
+Subkey-Curve: cv25519
 Name-Real: ${hostname}
 Name-Email: ${hostname}@localhost
 Expire-Date: 0
+%commit
 EOF
     GNUPGHOME="$gpgDir" gpg --armor --export > "$gpgDir/public.asc"
     git add "$gpgDir"
