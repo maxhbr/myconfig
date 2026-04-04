75529538058bdfc7a40135c97d166c75e5fe43fb
Maximilian Huber <gh@maxhbr.de>
2026-04-04 23:56:53 +0200

    hermes on other gemma
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.thing/services.llama-swap.nix
+++ hosts/host.thing/services.llama-swap.nix
@@ -79,7 +79,6 @@
           "CUDA0"
         ];
         aliases = [
-          "hermes"
           "gemma-4-26B-A4B-Q8"
         ];
         ttl = 300;
@@ -101,6 +100,7 @@
         name = "gemma-4-31B-it-Q6_K_XL";
         path = "/mnt/disk/models/gemma-4-31B-it-GGUF/gemma-4-31B-it-UD-Q6_K_XL.gguf";
         devices = [
+          "hermes"
           "Vulkan0"
           "Vulkan1"
           "CUDA0"
