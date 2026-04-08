50f4c575211184f5bd781cd4d382f7da911edcc8
Maximilian Huber <gh@maxhbr.de>
2026-04-08 08:09:41 +0200

    ollama
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.thing/ai.thing.nix
+++ hosts/host.thing/ai.thing.nix
@@ -200,46 +200,45 @@ in
       };
     };
 
-    systemd.tmpfiles.rules = [ "d /persistent/cache/ollama 0700 ollama ollama -" ];
+    # systemd.tmpfiles.rules = [ "d /persistent/cache/ollama 0700 ollama ollama -" ];
+    # services.ollama = {
+    #   enable = true;
+    #   home = "/persistent/cache/ollama";
+    #   environmentVariables = {
+    #     # OLLAMA_FLASH_ATTENTION = lib.mkForce "0";
+    #     OLLAMA_ORIGIN = "*";
+    #   };
 
-    services.ollama = {
-      enable = true;
-      home = "/persistent/cache/ollama";
-      environmentVariables = {
-        # OLLAMA_FLASH_ATTENTION = lib.mkForce "0";
-        OLLAMA_ORIGIN = "*";
-      };
+    #   openFirewall = false;
+    #   # host = "0.0.0.0";
 
-      openFirewall = false;
-      # host = "0.0.0.0";
-
-      # loadModels = [
-      #   # "hf.co/unsloth/Qwen3.5-122B-A10B-GGUF:Q6_K"
-      #   # "cogito:32b"
-      #   # "deepseek-r1:32b"
-      #   # "gemma3:27b"
-      #   # "glm-4.7-flash:q8_0"
-      #   # "glm-4.7-flash:bf16"
-      #   # "gpt-oss:20b"
-      #   # "gpt-oss:120b"
-      #   # "granite4"
-      #   # "llama4:16x17b"
-      #   # "llama3.2:3b"
-      #   # "llava:7b"
-      #   # "llava:34b"
-      #   # "magistral:24b"
-      #   # "openthinker:32b"
-      #   # "phi4"
-      #   # "qwen3-coder-next:q8_0"
-      #   # "qwen2.5vl:32b"
-      #   # "qwen3-vl:32b"
-      #   # "qwen3.5:122b"
-      #   # "qwen3:30b"
-      #   # "qwen3:32b"
-      #   # "qwq:32b"
-      #   # "smollm2:1.7b"
-      # ];
-    };
+    #   # loadModels = [
+    #   #   # "hf.co/unsloth/Qwen3.5-122B-A10B-GGUF:Q6_K"
+    #   #   # "cogito:32b"
+    #   #   # "deepseek-r1:32b"
+    #   #   # "gemma3:27b"
+    #   #   # "glm-4.7-flash:q8_0"
+    #   #   # "glm-4.7-flash:bf16"
+    #   #   # "gpt-oss:20b"
+    #   #   # "gpt-oss:120b"
+    #   #   # "granite4"
+    #   #   # "llama4:16x17b"
+    #   #   # "llama3.2:3b"
+    #   #   # "llava:7b"
+    #   #   # "llava:34b"
+    #   #   # "magistral:24b"
+    #   #   # "openthinker:32b"
+    #   #   # "phi4"
+    #   #   # "qwen3-coder-next:q8_0"
+    #   #   # "qwen2.5vl:32b"
+    #   #   # "qwen3-vl:32b"
+    #   #   # "qwen3.5:122b"
+    #   #   # "qwen3:30b"
+    #   #   # "qwen3:32b"
+    #   #   # "qwq:32b"
+    #   #   # "smollm2:1.7b"
+    #   # ];
+    # };
     # services.tabby = {
     #   enable = false;
     #   acceleration = "cuda";
