5747b7d49759e4726aabb3834a50a604e034e939
Maximilian Huber <gh@maxhbr.de>
2026-05-04 23:09:40 +0200

    aliases for litellm
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- hosts/host.thing/default.nix
+++ hosts/host.thing/default.nix
@@ -92,7 +92,20 @@
     networking.useDHCP = false;
     system.nixos.tags = config.myconfig.hardware.gpu.variant;
 
-    services.litellm.enable = true;
+    services.litellm = {
+      enable = true;
+      settings.router_settings = {
+        model_group_alias = {
+          "hermes" = "llama-swap-33656:hermes";
+          "hermes-fallback" = "llama-swap-33656:hermes-fallback";
+          "opencode-fast" ="llama-swap-33656:opencode-fast";
+          "opencode-fast-fallback" = "llama-swap-33656:opencode-fast-fallback";
+          "opencode" = "llama-swap-33657:opencode";
+          "opencode-slow" = "llama-swap-33657:opencode-slow";
+          "opencode-fallback" = "llama-swap-33657:opencode-fallback";
+        };
+      };
+    };
 
     myconfig = {
       persistence.impermanence = {
