e2a2ed5fc08ee7aeb9e1f6a68d060358c08c7ef6
Maximilian Huber <oss@maximilian-huber.de>
2026-08-18 14:11:59 +0200

    wyoming: list
    Signed-off-by: Maximilian Huber <oss@maximilian-huber.de>

--- hosts/host.thing/services.wyoming.nix
+++ hosts/host.thing/services.wyoming.nix
@@ -10,10 +10,9 @@
 }:
 {
   imports = [
-    {
+    (lib.mkIf config.services.wyoming.faster-whisper.servers.main.enable {
       # --- Wyoming Faster Whisper ---
       services.wyoming.faster-whisper.servers.main = {
-        enable = false; # Temporarily disabled
         model = "large-v3-turbo";
         language = "auto";
         device = "cuda";
@@ -31,23 +30,26 @@
           MemoryHigh = "14G";
         };
       };
-    }
-    {
+    })
+    (lib.mkIf config.services.wyoming.piper.servers.yoda.enable {
       # --- Wyoming Piper TTS ---
       services.wyoming.piper.servers.yoda = {
-        enable = false; # Temporarily disabled
         voice = "en-us-ryan-high";
         uri = "tcp://0.0.0.0:10200";
         useCUDA = true;
       };
 
     }
-    {
+    (lib.mkIf config.services.wyoming.openwakeword.enable {
       # --- Wyoming OpenWakeWord ---
       services.wyoming.openwakeword = {
-        enable = false; # Temporarily disabled
         uri = "tcp://0.0.0.0:10400";
       };
-    }
+    })
   ];
+  config = {
+    services.wyoming.faster-whisper.servers.main.enable = false;
+    services.wyoming.piper.servers.yoda.enable = false;
+    services.wyoming.openwakeword.enable = false;
+  };
 }
